e1cc02f8d7111cde6bc6e3332e20b8519ce9a20d
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-16 16:55:55 +0000

    Implement high-level logic for persisting events

--- includes/class-wporg-gp-translation-events-translation-listener.php
+++ includes/class-wporg-gp-translation-events-translation-listener.php
@@ -13,6 +13,39 @@ class WPORG_GP_Translation_Events_Translation_Listener {
 	}
 
 	private function handle_action( GP_Translation $translation, string $action_type ): void {
+		// Get events that are active now, for which the user is registered for.
+		$active_events = $this->get_active_events( new DateTime() );
+		$events = $this->select_events_user_is_registered_for( $active_events, $translation->user_id );
+
+		foreach ( $events as $event ) {
+			$this->persist( $translation, $event );
+		}
+	}
+
+	private function persist( GP_Translation $translation, WP_Post $event ): void {
 		// TODO.
+	}
+
+	/**
+	 * @return WP_Post[]
+	 */
+	private function get_active_events( DateTime $at ): array {
+		// TODO.
+		return [];
+	}
+
+	/**
+	 * @param WP_Post[] $events
+	 *
+	 * @return WP_Post[]
+	 */
+	private function select_events_user_is_registered_for( array $events, int $user_id ): array {
+		return array_filter(
+			$events,
+			function ( $event ) {
+				// TODO.
+				return true;
+			}
+		);
 	}
 }
