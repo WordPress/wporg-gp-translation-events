745a8bd2613e20e59df26d6574640cfc30a65381
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-16 15:07:18 +0000

    Call persist function when a translation is created

--- includes/class-wporg-gp-translation-events-translation-listener.php
+++ includes/class-wporg-gp-translation-events-translation-listener.php
@@ -1,7 +1,18 @@
 <?php
 
 class WPORG_GP_Translation_Events_Translation_Listener {
+	const ACTION_TYPE_CREATED = 'created';
+
 	function start(): void {
-		// TODO
+		add_action(
+			'gp_translation_created',
+			function ( $translation ) {
+				$this->persist( $translation, self::ACTION_TYPE_CREATED );
+			},
+		);
+	}
+
+	private function persist( GP_Translation $translation, string $action_type ): void {
+		// TODO.
 	}
 }
