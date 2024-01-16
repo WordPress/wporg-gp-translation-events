094b651d3f434b7fb79dec60773e7aa16d67344a
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-16 14:26:39 +0000

    Merge if statements

--- wporg-gp-translation-events.php
+++ wporg-gp-translation-events.php
@@ -190,9 +190,6 @@ add_action( 'save_post', 'save_event_meta_boxes' );
 
 if ( class_exists( GP_Route::class ) ) {
 	require_once __DIR__ . '/includes/class-wporg-gp-translation-events-route.php';
-}
-
-if ( class_exists( GP_Route::class ) ) {
 	GP::$router->prepend( '/events?', array( 'WPORG_GP_Translation_Events_Route', 'events_list' ), 'get' );
 	GP::$router->prepend( '/events/new', array( 'WPORG_GP_Translation_Events_Route', 'events_create' ), 'get' );
 	GP::$router->prepend( '/events/edit/(\d+)', array( 'WPORG_GP_Translation_Events_Route', 'events_edit' ), 'get' );
