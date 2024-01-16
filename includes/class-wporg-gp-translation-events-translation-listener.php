4de9aea7ba87713e667718cf388f46032894c536
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-16 16:52:52 +0000

    Rename method

--- includes/class-wporg-gp-translation-events-translation-listener.php
+++ includes/class-wporg-gp-translation-events-translation-listener.php
@@ -3,16 +3,16 @@
 class WPORG_GP_Translation_Events_Translation_Listener {
 	const ACTION_TYPE_CREATED = 'created';
 
-	function start(): void {
+	public function start(): void {
 		add_action(
 			'gp_translation_created',
 			function ( $translation ) {
-				$this->persist( $translation, self::ACTION_TYPE_CREATED );
+				$this->handle_action( $translation, self::ACTION_TYPE_CREATED );
 			},
 		);
 	}
 
-	private function persist( GP_Translation $translation, string $action_type ): void {
+	private function handle_action( GP_Translation $translation, string $action_type ): void {
 		// TODO.
 	}
 }
