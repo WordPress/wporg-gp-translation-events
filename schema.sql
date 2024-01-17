2b00ad93b84de6816fe68693be08f616c741ee4d
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-17 15:57:49 +0000

    Remove database name from schema definition

--- schema.sql
+++ schema.sql
@@ -9,10 +9,10 @@ create or replace table wp_wporg_gp_translation_events_actions
     comment 'Tracks translations that were created during a translation event';
 
 create index wp_wporg_gp_translation_events_actions_event_id_index
-    on wordpress.wp_wporg_gp_translation_events_actions (event_id);
+    on wp_wporg_gp_translation_events_actions (event_id);
 
 create index wp_wporg_gp_translation_events_actions_created_at_index
-    on wordpress.wp_wporg_gp_translation_events_actions (created_at);
+    on wp_wporg_gp_translation_events_actions (created_at);
 
 create index wp_wporg_gp_translation_events_actions_user_id_index
-    on wordpress.wp_wporg_gp_translation_events_actions (user_id);
+    on wp_wporg_gp_translation_events_actions (user_id);
