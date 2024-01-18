84490b4644adea4ec295dd8edd1bc4132e51c5c5
Tosin Oguntuyi <tcubed03@gmail.com>
2024-01-18 05:06:27 +0000

    Add placeholder content to event page

--- templates/event.php
+++ templates/event.php
@@ -3,3 +3,33 @@ gp_title( __( 'Translation Events - Create new event' ) );
 gp_tmpl_header();
 
 ?>
+
+<div class="event-details-page">
+<div class="event-details-head">
+        <h1>Translation Day 2024</h1>
+        <p>Host: <a href="#">Jane Doe</a></p>
+    </div>
+<div class="event-details-left">
+   
+    
+    <div class="event-page-content">
+        <p>
+            <img src="https://live.staticflickr.com/65535/52747811060_60e11ebafb_c.jpg" width="100%" />
+            <br>
+<strong>Description</strong><br>
+            Celebrate Translation Day with us, an event dedicated to the art of language bridging! Join linguists, professionals, and language enthusiasts on this vibrant occasion. <br>
+            Immerse yourself in a day of diverse linguistic exploration, featuring insightful talks, interactive workshops, and networking opportunities. Discover the magic of words and the pivotal role of translators in fostering global understanding. From cultural nuances to literary treasures, delve into the intricacies of language. Whether you're a seasoned translator or just curious, this event promises to be a linguistic feast. <br>
+            Embrace the power of words and join us on Translation Day for an enlightening journey through diverse languages and cultures.
+
+    </div>
+</div>
+    <div class="event-details-right">
+        <div class="event-details-date">
+            <p><span class="dashicons dashicons-calendar"></span> Thursday, January 18, 2024</p>
+            <p><span class="dashicons dashicons-clock"></span> 13:00 - 15:00</p>
+        </div>
+        <div class="event-details-join">
+            <button class="button is-primary" id="join-event">Attend Event</button>
+        </div>
+    </div>
+</div>
