88c94969e1b54ace66c52f5e9141d980247f2222
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-22 17:24:37 +0000

    Rename created_at to happened_at

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ create or replace table wp_wporg_gp_translation_events_actions
     event_id       int(10)     not null comment 'ID of the event',
     user_id        int(10)     not null comment 'ID of the user who authored the translation',
     translation_id int(10)     not null comment 'ID of the translation',
-    created_at     datetime    not null comment 'When the translation was created, in UTC',
+    happened_at    datetime    not null comment 'When the action happened, in UTC',
     locale         varchar(10) not null comment 'Locale of the translation'
 )
     comment 'Tracks translations that were created during a translation event';
@@ -11,8 +11,8 @@ create or replace table wp_wporg_gp_translation_events_actions
 create index wp_wporg_gp_translation_events_actions_event_id_index
     on wp_wporg_gp_translation_events_actions (event_id);
 
-create index wp_wporg_gp_translation_events_actions_created_at_index
-    on wp_wporg_gp_translation_events_actions (created_at);
+create index wp_wporg_gp_translation_events_actions_happened_at_index
+    on wp_wporg_gp_translation_events_actions (happened_at);
 
 create index wp_wporg_gp_translation_events_actions_user_id_index
     on wp_wporg_gp_translation_events_actions (user_id);
