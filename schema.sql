20aec8a0f38d169d32d0243089aedc9363105c33
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-22 17:48:49 +0000

    Add primary key

--- schema.sql
+++ schema.sql
@@ -1,13 +1,18 @@
+# This file is only used in development environments, and to serve as documentation.
+# It is not used in production in any way.
+
 create or replace table wp_wporg_gp_translation_events_actions
 (
     event_id       int(10)     not null comment 'ID of the event',
-    user_id        int(10)     not null comment 'ID of the user who authored the translation',
     translation_id int(10)     not null comment 'ID of the translation',
+    user_id        int(10)     not null comment 'ID of the user who authored the translation',
     happened_at    datetime    not null comment 'When the action happened, in UTC',
     action         varchar(16) not null comment 'The action that happened (created, rejected, etc)',
-    locale         varchar(10) not null comment 'Locale of the translation'
+    locale         varchar(10) not null comment 'Locale of the translation',
+    # Make sure that for a given event and translation, the user cannot do multiple actions of the same type.
+    primary key (event_id, translation_id, user_id, action)
 )
-    comment 'Tracks translations that were created during a translation event';
+    comment 'Tracks translation actions that happened during a translation event';
 
 create index wp_wporg_gp_translation_events_actions_event_id_index
     on wp_wporg_gp_translation_events_actions (event_id);
