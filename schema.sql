2bab7434c6754c83ed3eb1d05df63911a2a162ec
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-25 16:18:13 +0000

    Reduce length of action varchar

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ create or replace table wp_wporg_gp_translation_events_actions
     event_id       int(10)     not null comment 'ID of the event',
     translation_id int(10)     not null comment 'ID of the translation',
     user_id        int(10)     not null comment 'ID of the user who made the action',
-    action         varchar(16) not null comment 'The action that the user made (create, reject, etc)',
+    action         varchar(10) not null comment 'The action that the user made (create, reject, etc)',
     locale         varchar(10) not null comment 'Locale of the translation',
     happened_at    datetime    not null comment 'When the action happened, in UTC',
     # Make sure that for a given event and translation, the user cannot do multiple actions of the same type.
