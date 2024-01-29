32e574f2681cb555b3f10bae1f3aea431dc642b1
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-29 14:08:24 +0000

    Get event start and end

--- includes/class-wporg-gp-translation-events-stats-calculator.php
+++ includes/class-wporg-gp-translation-events-stats-calculator.php
@@ -12,7 +12,13 @@ class WPORG_GP_Translation_Events_Event_Stats {
 }
 
 class WPORG_GP_Translation_Events_Stats_Calculator {
+	/**
+	 * @throws Exception
+	 */
 	function for_event( WP_Post $event ): WPORG_GP_Translation_Events_Event_Stats {
+		$start = new DateTime( get_post_meta( $event->ID, '_event_start_date', true ), new DateTimeZone( 'UTC' ) );
+		$end   = new DateTime( get_post_meta( $event->ID, '_event_end_date', true ), new DateTimeZone( 'UTC' ) );
+
 		return new WPORG_GP_Translation_Events_Event_Stats( 42 );
 	}
 }
