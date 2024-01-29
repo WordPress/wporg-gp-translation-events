c7c87027486ebe46cd3bccac548ccb1e1889c009
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-29 11:58:54 +0000

    Fix indentation

--- templates/event.php
+++ templates/event.php
@@ -9,11 +9,11 @@ gp_tmpl_header();
 		<h1><?php echo esc_html( $event_title ); ?></h1>
 		<p>Host: <a href="<?php echo esc_attr( get_author_posts_url( $event->post_author ) ); ?>"><?php echo esc_html( get_the_author_meta( 'display_name', $event->post_author ) ); ?></a></p>
 	</div>
-<div class="event-details-left">
-	<div class="event-page-content">
-		<?php echo esc_html( $event_description ); ?>
+	<div class="event-details-left">
+		<div class="event-page-content">
+			<?php echo esc_html( $event_description ); ?>
+		</div>
 	</div>
-</div>
 	<div class="event-details-right">
 		<div class="event-details-date">
 			<p><span class="dashicons dashicons-calendar"></span> <?php echo esc_html( ( new DateTime( $event_start_date ) )->format( 'l, F j, Y' ) ); ?></p>
