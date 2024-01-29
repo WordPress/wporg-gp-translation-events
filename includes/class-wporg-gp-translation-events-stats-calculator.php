e759a3c0b48a016ec674a9c19b3763b80acf8e45
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-29 14:54:14 +0000

    Calculate create stats
    No pagination yet.

--- includes/class-wporg-gp-translation-events-stats-calculator.php
+++ includes/class-wporg-gp-translation-events-stats-calculator.php
@@ -12,6 +12,8 @@ class WPORG_GP_Translation_Events_Event_Stats {
 }
 
 class WPORG_GP_Translation_Events_Stats_Calculator {
+	private string $ACTIONS_TABLE_NAME = WPORG_GP_Translation_Events_Translation_Listener::ACTIONS_TABLE_NAME;
+
 	/**
 	 * @throws Exception
 	 */
@@ -19,6 +21,35 @@ class WPORG_GP_Translation_Events_Stats_Calculator {
 		$start = new DateTime( get_post_meta( $event->ID, '_event_start_date', true ), new DateTimeZone( 'UTC' ) );
 		$end   = new DateTime( get_post_meta( $event->ID, '_event_end_date', true ), new DateTimeZone( 'UTC' ) );
 
-		return new WPORG_GP_Translation_Events_Event_Stats( 42 );
+		global $wpdb;
+
+		$query = $wpdb->prepare(
+			"
+				select action
+				from $this->ACTIONS_TABLE_NAME
+				where event_id = %d
+				  and happened_at between cast('%s' as datetime) and cast('%s' as datetime)
+			",
+			[
+				$event->ID,
+				$start->format( 'Y-m-d H:i:s' ),
+				$end->format( 'Y-m-d H:i:s' ),
+			]
+		);
+
+		$created = 0;
+		$results = $wpdb->get_results( $query );
+		foreach ( $results as $result ) {
+			switch ( $result->action ) {
+				case WPORG_GP_Translation_Events_Translation_Listener::ACTION_CREATE:
+					$created ++;
+					break;
+				default:
+					// Unknown action. Should not happen.
+					break;
+			}
+		}
+
+		return new WPORG_GP_Translation_Events_Event_Stats( $created );
 	}
 }
