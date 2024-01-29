d6f8774e348bd0e12012da78b0b90495d2d4d8fe
Paulo Pinto <paulo.pinto@automattic.com>
2024-01-29 15:54:43 +0000

    Use correct field names

--- includes/class-wporg-gp-translation-events-stats-calculator.php
+++ includes/class-wporg-gp-translation-events-stats-calculator.php
@@ -48,8 +48,8 @@ class WPORG_GP_Translation_Events_Stats_Calculator {
 	 * @throws Exception
 	 */
 	function for_event( WP_Post $event ): WPORG_GP_Translation_Events_Event_Stats {
-		$start = new DateTime( get_post_meta( $event->ID, '_event_start_date', true ), new DateTimeZone( 'UTC' ) );
-		$end   = new DateTime( get_post_meta( $event->ID, '_event_end_date', true ), new DateTimeZone( 'UTC' ) );
+		$start = new DateTime( get_post_meta( $event->ID, '_event_start', true ), new DateTimeZone( 'UTC' ) );
+		$end   = new DateTime( get_post_meta( $event->ID, '_event_end', true ), new DateTimeZone( 'UTC' ) );
 
 		global $wpdb;
 
