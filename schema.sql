3707fea3e05708f8e923c2c4c16230985e39f796
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-07 15:17:40 +0000

    Change order of primary key so that user_id comes first

--- schema.sql
+++ schema.sql
@@ -4,11 +4,11 @@
 create or replace table wp_wporg_gp_translation_events_actions
 (
     event_id       int(10)     not null comment 'ID of the event',
-    translation_id int(10)     not null comment 'ID of the translation',
     user_id        int(10)     not null comment 'ID of the user who made the action',
+    translation_id int(10)     not null comment 'ID of the translation',
     action         varchar(16) not null comment 'The action that the user made (create, reject, etc)',
     locale         varchar(10) not null comment 'Locale of the translation',
     # Make sure that for a given event and translation, the user cannot do multiple actions.
-    primary key (event_id, translation_id, user_id)
+    primary key (event_id, user_id, translation_id)
 )
     comment 'Tracks translation actions that happened during a translation event';
