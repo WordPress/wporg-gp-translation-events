3514555d275980a0b15a525085b3c88f8170dce4
Tosin Oguntuyi <tcubed03@gmail.com>
2024-02-07 13:20:14 +0100

    Remove colon in label and reposition event url

--- templates/events-form.php
+++ templates/events-form.php
@@ -5,7 +5,6 @@ gp_tmpl_header();
 ?>
 
 <h2  class="event-page-title"><?php echo esc_html( $event_form_title ); ?></h2>
-<p id="event-url" class="<?php echo esc_attr( $css_show_url ); ?>"><span>Event URL:</span> <a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_url( $permalink ); ?></a></p>
 <form class="translation-event-form" action="" method="post">
 	<?php wp_nonce_field( '_event_nonce', '_event_nonce' ); ?>
 	<input type="hidden" name="action" value="submit_event_ajax">
@@ -13,23 +12,27 @@ gp_tmpl_header();
 	<input type="hidden" id="event-id" name="event_id" value="<?php echo esc_attr( $event_id ); ?>">
 	<input type="hidden" id="event-form-action" name="event_form_action">
 	<div>
-		<label for="event-title">Event Title:</label>
+		<label for="event-title">Event Title</label>
 		<input type="text" id="event-title" name="event_title" value="<?php echo esc_html( $event_title ); ?>" required>
 	</div>
+    <p id="event-url" class="<?php echo esc_attr( $css_show_url ); ?>">
+        <span>Event URL</span>
+         <a class="event-permalink" href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_url( $permalink ); ?></a>
+    </p>
 	<div>
-		<label for="event-description">Event Description:</label>
+		<label for="event-description">Event Description</label>
 		<textarea id="event-description" name="event_description" rows="4" required><?php echo esc_html( $event_description ); ?></textarea>
 	</div>
 	<div>
-		<label for="event-start">Start Date:</label>
+		<label for="event-start">Start Date</label>
 		<input type="datetime-local" id="event-start" name="event_start" value="<?php echo esc_attr( $event_start ); ?>" required>
 	</div>
 	<div>
-		<label for="event-end">End Date:</label>
+		<label for="event-end">End Date</label>
 		<input type="datetime-local" id="event-end" name="event_end" value="<?php echo esc_attr( $event_end ); ?>" required>
 	</div>
 	<div>
-		<label for="event-timezone">Event Timezone:</label>
+		<label for="event-timezone">Event Timezone</label>
 		<select id="event-timezone" name="event_timezone"  required>
 			<?php
 			echo wp_kses(
@@ -46,11 +49,11 @@ gp_tmpl_header();
 		</select>
 	</div>
 	<div>
-		<label for="event-locale">Locale:</label>
+		<label for="event-locale">Locale</label>
 		<input type="text" id="event-locale" name="event_locale" value="<?php echo esc_attr( $event_locale ); ?>">
 	</div>
 	<div>
-		<label for="event-project-name">Project Name:</label>
+		<label for="event-project-name">Project Name</label>
 		<input type="text" id="event-project-name" name="event_project_name" value="<?php echo esc_attr( $event_project_name ); ?>">
 	</div>
 	<div class="submit-btn-group">
