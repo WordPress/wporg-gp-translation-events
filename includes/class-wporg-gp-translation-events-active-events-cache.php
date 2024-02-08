3cd16d69fef8455aa17f8a7f30ecf648671a7346
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-08 12:08:26 +0000

    Don't listen to update_post_metadata
    We'll do this in a different way.

--- includes/class-wporg-gp-translation-events-active-events-cache.php
+++ includes/class-wporg-gp-translation-events-active-events-cache.php
@@ -15,33 +15,6 @@ class WPORG_GP_Translation_Events_Active_Events_Cache_Entry {
 class WPORG_GP_Translation_Events_Active_Events_Cache {
 	public const CACHE_DURATION = 60 * 60 * 24; // 24 hours.
 	private const KEY = 'translation-events-active-events';
-	private static bool $invalidated_on_event_save = false;
-
-	public function __construct() {
-		// Invalidate cache when events are modified.
-		add_filter(
-			'update_post_metadata',
-			function ( $ignore, int $object_id, string $meta_key ) {
-				// When an event is saved, the hook will be called for each meta property of the event.
-				// However, we only need to invalidate the cache once, so we check first if we already did it.
-				if ( self::$invalidated_on_event_save ) {
-					return;
-				}
-				$event_meta_keys = [ '_event_start', '_event_end', '_event_timezone' ];
-				if ( ! in_array( $meta_key, $event_meta_keys ) ) {
-					return;
-				}
-				try {
-					$this->invalidate();
-					self::$invalidated_on_event_save = true;
-				} catch ( Exception $exception ) {
-					error_log( $exception );
-				}
-			},
-			10,
-			3
-		);
-	}
 
 	/**
 	 * @param WPORG_GP_Translation_Events_Active_Events_Cache_Entry[] $cache_entries
