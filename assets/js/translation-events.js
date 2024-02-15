ccc6de5535b8ab51ddd6ae830847118796321f6b
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-15 16:08:56 +0000

    Extract submit logic to a function

--- assets/js/translation-events.js
+++ assets/js/translation-events.js
@@ -14,51 +14,7 @@
 					'click',
 					function ( e ) {
 						e.preventDefault();
-						if ( $( '#event-end' ).val() <= $( '#event-start' ).val() ) {
-							$gp.notices.error( 'Event end date and time must be later than event start date and time.' );
-							return;
-						}
-						const btnClicked = $( this ).data( 'event-status' );
-						if ( btnClicked === 'publish' && '' === $( '#event-id' ).val() ) {
-							const submitPrompt = 'Are you sure you want to publish this event?';
-							if ( ! confirm( submitPrompt ) ) {
-								return;
-							}
-						}
-						$( '#event-form-action' ).val( btnClicked );
-						const $form = $( '.translation-event-form' );
-						const $is_creation = $('#form-name').val() === 'create_event';
-
-						$.ajax(
-							{
-								type: 'POST',
-								url: $translation_event.url,
-								data:$form.serialize(),
-								success: function(response) {
-									if ( response.data.eventId ) {
-										history.replaceState( '', '', response.data.eventEditUrl );
-										$('#form-name').val('edit_event');
-										$('.event-page-title').text('Edit Event');
-										$('#event-id').val(response.data.eventId);
-										if( btnClicked === 'publish' ) {
-											$('button[data-event-status="draft"]').hide();
-											$('button[data-event-status="publish"]').text('Update Event');
-										}
-										if( btnClicked === 'draft' ) {
-											$('button[data-event-status="draft"]').text('Update Draft');
-										}
-										$('#event-url').removeClass('hide-event-url').find('a').attr('href', response.data.eventUrl).text(response.data.eventUrl);
-										if ( $is_creation ) {
-											$('#delete-button').toggle();
-										}
-										$gp.notices.success(response.data.message);
-									}
-								},
-								error: function(error) {
-									$gp.notices.error(response.data.message);
-								}
-							}
-						);
+						handleSubmit();
 					}
 				);
 
@@ -84,6 +40,54 @@
 				});
 			}
 		);
+
+		function handleSubmit() {
+			if ( $( '#event-end' ).val() <= $( '#event-start' ).val() ) {
+				$gp.notices.error( 'Event end date and time must be later than event start date and time.' );
+				return;
+			}
+			const btnClicked = $( this ).data( 'event-status' );
+			if ( btnClicked === 'publish' && '' === $( '#event-id' ).val() ) {
+				const submitPrompt = 'Are you sure you want to publish this event?';
+				if ( ! confirm( submitPrompt ) ) {
+					return;
+				}
+			}
+			$( '#event-form-action' ).val( btnClicked );
+			const $form = $( '.translation-event-form' );
+			const $is_creation = $('#form-name').val() === 'create_event';
+
+			$.ajax(
+				{
+					type: 'POST',
+					url: $translation_event.url,
+					data:$form.serialize(),
+					success: function(response) {
+						if ( response.data.eventId ) {
+							history.replaceState( '', '', response.data.eventEditUrl );
+							$('#form-name').val('edit_event');
+							$('.event-page-title').text('Edit Event');
+							$('#event-id').val(response.data.eventId);
+							if( btnClicked === 'publish' ) {
+								$('button[data-event-status="draft"]').hide();
+								$('button[data-event-status="publish"]').text('Update Event');
+							}
+							if( btnClicked === 'draft' ) {
+								$('button[data-event-status="draft"]').text('Update Draft');
+							}
+							$('#event-url').removeClass('hide-event-url').find('a').attr('href', response.data.eventUrl).text(response.data.eventUrl);
+							if ( $is_creation ) {
+								$('#delete-button').toggle();
+							}
+							$gp.notices.success(response.data.message);
+						}
+					},
+					error: function(error) {
+						$gp.notices.error(response.data.message);
+					}
+				}
+			);
+		}
 
 		function validateEventDates() {
 			const startDateTimeInput = $('#event-start');
