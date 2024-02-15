cadc5f14f070bfbb309bd518d34b2b71be2ea1cd
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-15 13:44:04 +0000

    Lint event page template

--- templates/event.php
+++ templates/event.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Template for event page.
+ */
 
 /** @var WP_Post $event */
 /** @var string $event_title */
@@ -9,8 +12,7 @@
 /* translators: %s: Event title. */
 gp_title( sprintf( __( 'Translation Events - %s' ), esc_html( $event_title ) ) );
 gp_tmpl_header();
-gp_tmpl_load( 'events-header', get_defined_vars(), dirname( __FILE__ ) );
-
+gp_tmpl_load( 'events-header', get_defined_vars(), __DIR__ );
 ?>
 
 <div class="event-details-page">
@@ -32,19 +34,19 @@ gp_tmpl_load( 'events-header', get_defined_vars(), dirname( __FILE__ ) );
 		<div class="event-details-date">
 			<p><span class="dashicons dashicons-clock"></span> <time class="event-utc-time" datetime="<?php echo esc_attr( $event_start ); ?>"></time> - <time class="event-utc-time" datetime="<?php echo esc_attr( $event_end ); ?>"></time></p>
 		</div>
-		<?php  if ( is_user_logged_in() ) : ?>
+		<?php if ( is_user_logged_in() ) : ?>
 		<div class="event-details-join">
-			<form class="event-details-attend" method="post" action="<?php echo esc_url( gp_url( "/events/attend/$event_id" ) )?>">
-				<?php if ( ! $user_is_attending ): ?>
+			<form class="event-details-attend" method="post" action="<?php echo esc_url( gp_url( "/events/attend/$event_id" ) ); ?>">
+				<?php if ( ! $user_is_attending ) : ?>
 					<input type="submit" class="button is-primary" value="Attend Event"/>
-				<?php else: ?>
+				<?php else : ?>
 					<input type="submit" class="button is-secondary" value="You're attending"/>
 				<?php endif ?>
 			</form>
 		</div>
 		<?php endif; ?>
 	</div>
-<?php if ( ! empty( $event_stats->rows() ) ):?>
+<?php if ( ! empty( $event_stats->rows() ) ) : ?>
 	<div class="event-details-stats">
 		<h2>Stats</h2>
 		<table>
@@ -57,20 +59,20 @@ gp_tmpl_load( 'events-header', get_defined_vars(), dirname( __FILE__ ) );
 			</tr>
 			</thead>
 			<tbody>
-			<?php /** @var $row WPORG_GP_Translation_Events_Stats_Row */?>
-		<?php foreach ( $event_stats->rows() as $locale => $row ): ?>
+		<?php /** @var $row WPORG_GP_Translation_Events_Stats_Row */ ?>
+		<?php foreach ( $event_stats->rows() as $locale_ => $row ) : ?>
 			<tr>
-				<td><?php echo esc_html( $locale ) ?></td>
-				<td><?php echo esc_html( $row->created ) ?></td>
-				<td><?php echo esc_html( $row->reviewed ) ?></td>
-				<td><?php echo esc_html( $row->users ) ?></td>
+				<td><?php echo esc_html( $locale_ ); ?></td>
+				<td><?php echo esc_html( $row->created ); ?></td>
+				<td><?php echo esc_html( $row->reviewed ); ?></td>
+				<td><?php echo esc_html( $row->users ); ?></td>
 			</tr>
 		<?php endforeach ?>
 			<tr class="event-details-stats-totals">
 				<td>Total</td>
-				<td><?php echo esc_html( $event_stats->totals()->created ) ?></td>
-				<td><?php echo esc_html( $event_stats->totals()->reviewed ) ?></td>
-				<td><?php echo esc_html( $event_stats->totals()->users ) ?></td>
+				<td><?php echo esc_html( $event_stats->totals()->created ); ?></td>
+				<td><?php echo esc_html( $event_stats->totals()->reviewed ); ?></td>
+				<td><?php echo esc_html( $event_stats->totals()->users ); ?></td>
 			</tr>
 			</tbody>
 		</table>
