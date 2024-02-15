5976bdd482c85b1021f265eafef35392091149b8
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2024-02-15 13:05:42 +0100

    Fixes style

--- wporg-gp-translation-events.php
+++ wporg-gp-translation-events.php
@@ -299,22 +299,23 @@ add_filter( 'gp_nav_menu_items', 'gp_event_nav_menu_items' );
 /**
  * Generate a slug for the event post type when we save a draft event.
  *
+ * Generate a slug based on the event title if it's not provided.
+ *
  * @param array $data    An array of slashed post data.
  * @param array $postarr An array of sanitized, but otherwise unmodified post data.
  * @return array The modified post data.
  */
 function generate_event_slug( $data, $postarr ) {
-	if ( $data[ 'post_type' ] === 'event' && $data[ 'post_status' ] === 'draft' ) {
-		// Generate a slug based on the event title if it's not provided
-		if ( empty( $data[ 'post_name' ] ) ) {
-			$data[ 'post_name' ] = sanitize_title( $data[ 'post_title' ] );
+	if ( 'event' === $data['post_type'] && 'draft' === $data['post_status'] ) {
+		if ( empty( $data['post_name'] ) ) {
+			$data['post_name'] = sanitize_title( $data['post_title'] );
 		}
 	}
 
 	return $data;
 }
 
-add_filter('wp_insert_post_data', 'generate_event_slug', 10, 2);
+add_filter( 'wp_insert_post_data', 'generate_event_slug', 10, 2 );
 
 add_action(
 	'gp_init',
