1558784805d577e2a4c53db7eeb40b9efc04898f
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-19 14:26:26 +0000

    Add stats listener test

---
+++ tests/stats-listener.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wporg\tests;
+
+use WP_Ajax_UnitTestCase;
+
+class Stats_Listener_Test extends WP_Ajax_UnitTestCase {
+	public function test_stores_translation_created() {
+		$this->assertTrue( true );
+	}
+}
