3c3f3b262064cb6073eb7f862a364fcf2c715e25
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-20 12:40:29 +0000

    Create translation

--- tests/stats-listener.php
+++ tests/stats-listener.php
@@ -14,6 +14,36 @@ class Stats_Listener_Test extends GP_UnitTestCase_Route {
 	}
 
 	public function test_stores_translation_created() {
-		$this->assertTrue( true );
+		$this->event_factory->create();
+
+		$this->set_normal_user_as_current();
+		$user_id = wp_get_current_user()->ID;
+
+		$set      = $this->factory->translation_set->create_with_project_and_locale();
+		$original = $this->factory->original->create(
+			array(
+				'project_id' => $set->project->id,
+				'status'     => '+active',
+				'singular'   => 'foo',
+			)
+		);
+
+		$translation = $this->factory->translation->create(
+			array(
+				'user_id'            => $user_id,
+				'translation_set_id' => $set->id,
+				'original_id'        => $original->id,
+				'status'             => 'waiting',
+			)
+		);
+
+		global $wpdb;
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
+		$rows = $wpdb->get_results( 'select * from wp_wporg_gp_translation_events_actions' );
+		// phpcs:enable
+
+		// TODO.
+		$this->assertNotEmpty( $rows );
 	}
 }
