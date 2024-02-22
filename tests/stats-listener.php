266c574e532378522d8bcaf3880c40a764110b5b
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-22 14:38:37 +0000

    Test stats values

--- tests/stats-listener.php
+++ tests/stats-listener.php
@@ -19,7 +19,7 @@ class Stats_Listener_Test extends GP_UnitTestCase {
 		global $wpdb;
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
-		return $wpdb->get_results( 'select * from wp_wporg_gp_translation_events_actions' );
+		return $wpdb->get_results( 'select * from wp_wporg_gp_translation_events_actions', ARRAY_A );
 		// phpcs:enable
 	}
 
@@ -33,15 +33,20 @@ class Stats_Listener_Test extends GP_UnitTestCase {
 
 	public function test_stores_action_create() {
 		$this->set_normal_user_as_current();
-		$user_id = wp_get_current_user()->ID;
+		$user_id  = wp_get_current_user()->ID;
+		$event_id = $this->event_factory->create_active( array( $user_id ) );
 
-		$event_id    = $this->event_factory->create_active( array( $user_id ) );
 		$translation = $this->translation_factory->create( $user_id );
+		// Stats_Listener will have been called.
 
 		$stats = $this->get_stats();
-
-		// TODO.
-		$this->assertNotEmpty( $stats );
+		$this->assertCount( 1, $stats );
+		$stats = $stats[0];
+		$this->assertEquals( $event_id, $stats['event_id'] );
+		$this->assertEquals( $user_id, $stats['user_id'] );
+		$this->assertEquals( $translation->id, $stats['translation_id'] );
+		$this->assertEquals( 'create', $stats['action'] );
+		$this->assertEquals( 'aa', $stats['locale'] );
 	}
 
 	public function test_stores_action_approve() {
