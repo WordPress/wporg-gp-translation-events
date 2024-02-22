c7d38130e3f7e45e5d9c376010a4e8e0dea74f0b
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-22 14:25:22 +0000

    Add translation factory

--- tests/stats-listener.php
+++ tests/stats-listener.php
@@ -4,13 +4,15 @@ namespace Wporg\Tests;
 
 use GP_UnitTestCase;
 use Wporg\TranslationEvents\Tests\Event_Factory;
+use Wporg\TranslationEvents\Tests\Translation_Factory;
 
 class Stats_Listener_Test extends GP_UnitTestCase {
 	private Event_Factory $event_factory;
 
 	public function setUp(): void {
 		parent::setUp();
-		$this->event_factory = new Event_Factory();
+		$this->translation_factory = new Translation_Factory( $this->factory );
+		$this->event_factory       = new Event_Factory();
 	}
 
 	public function test_does_not_store_action_for_inactive_event() {
@@ -25,25 +27,8 @@ class Stats_Listener_Test extends GP_UnitTestCase {
 		$this->set_normal_user_as_current();
 		$user_id = wp_get_current_user()->ID;
 
-		$this->event_factory->create_active( array( $user_id ) );
-
-		$set      = $this->factory->translation_set->create_with_project_and_locale();
-		$original = $this->factory->original->create(
-			array(
-				'project_id' => $set->project->id,
-				'status'     => '+active',
-				'singular'   => 'foo',
-			)
-		);
-
-		$translation = $this->factory->translation->create(
-			array(
-				'user_id'            => $user_id,
-				'translation_set_id' => $set->id,
-				'original_id'        => $original->id,
-				'status'             => 'waiting',
-			)
-		);
+		$event_id    = $this->event_factory->create_active( array( $user_id ) );
+		$translation = $this->translation_factory->create( $user_id );
 
 		global $wpdb;
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
