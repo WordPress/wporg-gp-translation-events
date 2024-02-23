f1649afc50bf6d86b9efc6ec7d7526e8ebf1adef
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-23 14:52:51 +0000

    Add a Stats_Factory

---
+++ tests/lib/stats-factory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Wporg\TranslationEvents\Tests;
+
+use wpdb;
+
+class Stats_Factory {
+	private wpdb $wpdb;
+
+	public function __construct() {
+		global $wpdb;
+		$this->wpdb = $wpdb;
+	}
+
+	public function clean() {
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
+		$this->wpdb->query( 'delete from wp_wporg_gp_translation_events_actions' );
+		// phpcs:enable
+	}
+
+	public function create( int $event_id, $user_id, $translation_id, $action, $locale = 'aa' ) {
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery
+		$this->wpdb->insert(
+			'wp_wporg_gp_translation_events_actions',
+			array(
+				'event_id'       => $event_id,
+				'user_id'        => $user_id,
+				'translation_id' => $translation_id,
+				'action'         => $action,
+				'locale'         => $locale,
+			)
+		);
+	}
+
+	public function get_all(): array {
+		global $wpdb;
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
+		return $wpdb->get_results( 'select * from wp_wporg_gp_translation_events_actions', ARRAY_A );
+		// phpcs:enable
+	}
+}
