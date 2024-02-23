4d6a81e94710566f503e1dd54481020c18b5fc9e
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-23 14:32:18 +0000

    No longer need the run-tests.sh script

--- bin/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This is only for use in development environments, it's not used in CI.
-# You can call this with: composer dev:test
-
-set -ex
-
-wp-env run tests-cli --env-cwd=wp-content/plugins/wporg-gp-translation-events ./vendor/bin/phpunit
