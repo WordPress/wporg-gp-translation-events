b82e5d73e02c176d9e08102a7123f0e46c122209
Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>
2024-02-24 10:53:44 +0100

    Add the "Events I attended" view (#122)
    * Add the "Events I attended" view
    
    * Add a separator between the first two items in the top menu
    
    * Rename the events-user-created template to events-my-events
    
    * Remove the "Events I attended" from the top-right menu
    
    * Rename a method in the route and delete another route
    
    * Set all 'posts_per_page' to 10
    
    * Rename a method in the route and delete another route
    
    * Remove the 'events_i_attended' method
    
    * Remove some unnecessary variables
    
    * Remove the 'events-i-attended' template

--- includes/route.php
+++ includes/route.php
@@ -305,27 +305,72 @@ class Route extends GP_Route {
 	 *
 	 * @return void
 	 */
-	public function events_user_created() {
+	public function my_events() {
 		if ( ! is_user_logged_in() ) {
 			$this->die_with_error( 'You must be logged in to your events', 403 );
 		}
 		include ABSPATH . 'wp-admin/includes/post.php';
 
-		$user_id = get_current_user_id();
-		$_paged  = ( get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1;
-		$args    = array(
-			'post_type'      => 'event',
-			'posts_per_page' => 10,
-			'post_status'    => array( 'publish', 'draft' ),
-			'paged'          => $_paged,
-			'author'         => $user_id,
+		$_events_i_created_paged  = 1;
+		$_events_i_attended_paged = 1;
+
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
+		if ( isset( $_GET['events_i_created_paged'] ) ) {
+			$value = sanitize_text_field( wp_unslash( $_GET['events_i_created_paged'] ) );
+			if ( is_numeric( $value ) ) {
+				$_events_i_created_paged = (int) $value;
+			}
+		}
+		if ( isset( $_GET['events_i_attended_paged'] ) ) {
+			$value = sanitize_text_field( wp_unslash( $_GET['events_i_attended_paged'] ) );
+			if ( is_numeric( $value ) ) {
+				$_events_i_attended_paged = (int) $value;
+			}
+		}
+		// phpcs:enable
+
+		$user_id              = get_current_user_id();
+		$events               = get_user_meta( $user_id, self::USER_META_KEY_ATTENDING, true ) ?: array();
+		$events               = array_keys( $events );
+		$current_datetime_utc = ( new DateTime( 'now', new DateTimeZone( 'UTC' ) ) )->format( 'Y-m-d H:i:s' );
+		$args                 = array(
+			'post_type'              => 'event',
+			'posts_per_page'         => 10,
+			'events_i_created_paged' => $_events_i_created_paged,
+			'paged'                  => $_events_i_created_paged,
+			'post_status'            => array( 'publish', 'draft' ),
+			'author'                 => $user_id,
 			// phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
-			'meta_key'       => '_event_start',
-			'orderby'        => 'meta_value',
-			'order'          => 'DESC',
+			'meta_key'               => '_event_start',
+			'orderby'                => 'meta_value',
+			'order'                  => 'DESC',
 		);
-		$query = new WP_Query( $args );
-		$this->tmpl( 'events-user-created', get_defined_vars() );
+		$events_i_created_query = new WP_Query( $args );
+
+		$args = array(
+			'post_type'               => 'event',
+			'posts_per_page'          => 10,
+			'events_i_attended_paged' => $_events_i_attended_paged,
+			'paged'                   => $_events_i_attended_paged,
+			'post_status'             => 'publish',
+			'post__in'                => $events,
+			// phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
+			'meta_query'              => array(
+				array(
+					'key'     => '_event_end',
+					'value'   => $current_datetime_utc,
+					'compare' => '<',
+					'type'    => 'DATETIME',
+				),
+			),
+			// phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+			'meta_key'                => '_event_end',
+			'orderby'                 => 'meta_value',
+			'order'                   => 'DESC',
+		);
+		$events_i_attended_query = new WP_Query( $args );
+
+		$this->tmpl( 'events-my-events', get_defined_vars() );
 	}
 
 	/**
