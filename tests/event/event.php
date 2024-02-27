65a4cd7567a580893b106aaf9bc8dbd7fc3c0211
Paulo Pinto <paulo.pinto@automattic.com>
2024-02-27 12:35:13 +0000

    Validate end is after start

--- tests/event/event.php
+++ tests/event/event.php
@@ -2,7 +2,27 @@
 
 namespace Wporg\Tests\Event;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use WP_UnitTestCase;
+use Wporg\TranslationEvents\Event\Event;
+use Wporg\TranslationEvents\Event\InvalidStartOrEnd;
 
 class Event_Test extends WP_UnitTestCase {
+	public function test_validates_start_and_end() {
+		$timezone = new DateTimeZone( 'Europe/Lisbon' );
+		$now      = new DateTimeImmutable( 'now', $timezone );
+
+		$this->expectException( InvalidStartOrEnd::class );
+		new Event(
+			1,
+			$now,
+			$now->modify( '-1 hours' ),
+			$timezone,
+			'',
+			'',
+			'',
+			'',
+		);
+	}
 }
