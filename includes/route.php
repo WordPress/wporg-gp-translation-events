beb805f03be885a3dd1e4ed5ca84d24a8dbd7196
Tosin Oguntuyi <tcubed03@gmail.com>
2024-02-29 15:22:29 +0100

    Check if it's not published

--- includes/route.php
+++ includes/route.php
@@ -247,7 +247,7 @@ class Route extends GP_Route {
 		if ( ! $event ) {
 			$this->die_with_404();
 		}
-		if ( 'draft' === $event->post_status && intval( $event->post_author ) !== $user->ID ) {
+		if ( 'publish' !== $event->post_status && intval( $event->post_author ) !== $user->ID ) {
 			$this->die_with_error( esc_html__( 'You are not authorized to view this page.', 'gp-translation-events' ), 403 );
 		}
 
