46e45b1d7eba965e55e67896d32c207d403b8579
Alex Kirk <akirk@users.noreply.github.com>
2024-03-05 14:14:15 +0100

    Use gp_table_prefix instead of base_prefix (#151)
    * Use gp_table_prefix instead of base_prefix
    
    * lint
    
    * Forgotten global
    
    * Allow interpolation
    
    * Remove the unused DB_TABLE constant
    
    Co-authored-by: Paulo Pinto <paulo.pinto@automattic.com>
    
    * Fix styles
    
    ---------
    
    Co-authored-by: Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>
    Co-authored-by: Paulo Pinto <paulo.pinto@automattic.com>

--- tests/lib/stats-factory.php
+++ tests/lib/stats-factory.php
@@ -6,19 +6,21 @@ use wpdb;
 
 class Stats_Factory {
 	public function clean() {
-		global $wpdb;
+		global $wpdb, $gp_table_prefix;
 
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
-		$wpdb->query( "delete from {$wpdb->base_prefix}event_actions" );
+		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		$wpdb->query( "delete from {$gp_table_prefix}event_actions" );
 		// phpcs:enable
 	}
 
 	public function create( int $event_id, $user_id, $original_id, $action, $locale = 'aa' ) {
-		global $wpdb;
+		global $wpdb, $gp_table_prefix;
 		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery
 		$wpdb->insert(
-			$wpdb->base_prefix . 'event_actions',
+			// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+			$gp_table_prefix . 'event_actions',
 			array(
 				'event_id'    => $event_id,
 				'user_id'     => $user_id,
@@ -30,18 +32,20 @@ class Stats_Factory {
 	}
 
 	public function get_count(): int {
-		global $wpdb;
+		global $wpdb, $gp_table_prefix;
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
-		return intval( $wpdb->get_var( "select count(*) from {$wpdb->base_prefix}event_actions" ) );
+		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		return intval( $wpdb->get_var( "select count(*) from {$gp_table_prefix}event_actions" ) );
 		// phpcs:enable
 	}
 
 	public function get_by_event_id( $event_id ): array {
-		global $wpdb;
+		global $wpdb, $gp_table_prefix;
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
 		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
-		return $wpdb->get_row( $wpdb->prepare( "select * from {$wpdb->base_prefix}event_actions where event_id = %s", $event_id ), ARRAY_A );
+		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		return $wpdb->get_row( $wpdb->prepare( "select * from {$gp_table_prefix}event_actions where event_id = %s", $event_id ), ARRAY_A );
 		// phpcs:enable
 	}
 }
