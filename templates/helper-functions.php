fbfe2a3fba53c8c97d814a79520df28108e1fe03
Paulo Pinto <paulo.pinto@automattic.com>
2024-03-06 14:35:50 +0000

    gp_breadcrumb_translation_events can return null

--- templates/helper-functions.php
+++ templates/helper-functions.php
@@ -4,9 +4,9 @@
  *
  * @param array $extra_items Array of additional items to add to the breadcrumb.
  *
- * @return string HTML of the breadcrumb.
+ * @return ?string HTML of the breadcrumb.
  */
-function gp_breadcrumb_translation_events( array $extra_items = array() ): string {
+function gp_breadcrumb_translation_events( array $extra_items = array() ): ?string {
 	$breadcrumb = array(
 		empty( $extra_items ) ? __( 'Events', 'gp-translation-events' ) : gp_link_get( gp_url( '/events' ), __( 'Events', 'gp-translation-events' ) ),
 	);
