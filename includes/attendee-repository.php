1786e6ce2da213576603e1e1ca254234f9b8f96f
Paulo Pinto <paulo.pinto@automattic.com>
2024-03-11 12:35:06 +0000

    Define public interface of Attendee_Repository

--- includes/attendee-repository.php
+++ includes/attendee-repository.php
@@ -3,5 +3,24 @@
 namespace Wporg\TranslationEvents;
 
 class Attendee_Repository {
+	public function add_attendee( int $event_id, int $user_id ): void {
+		// TODO.
+	}
 
+	public function remove_attendee( int $event_id, int $user_id ): void {
+		// TODO.
+	}
+
+	public function is_attending( int $event_id, int $user_id ): bool {
+		// TODO.
+		return false;
+	}
+
+	/**
+	 * @return int[] User ids.
+	 */
+	public function get_attendees( int $event_id ): array { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
+		// TODO.
+		return array();
+	}
 }
