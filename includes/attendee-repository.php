a58d2a6a33a393c1c6242d8cce24fa299d65799d
Paulo Pinto <paulo.pinto@automattic.com>
2024-03-11 12:50:18 +0000

    Implement add_attendee

--- includes/attendee-repository.php
+++ includes/attendee-repository.php
@@ -2,11 +2,28 @@
 
 namespace Wporg\TranslationEvents;
 
+use Exception;
+
 class Attendee_Repository {
 	public const USER_META_KEY = 'translation-events-attending';
 
+	/**
+	 * @throws Exception
+	 */
 	public function add_attendee( int $event_id, int $user_id ): void {
-		// TODO.
+		if ( $event_id < 1 ) {
+			throw new Exception( 'invalid event id' );
+		}
+		if ( $user_id < 1 ) {
+			throw new Exception( 'invalid user id' );
+		}
+
+		$event_ids = get_user_meta( $user_id, self::USER_META_KEY, true );
+		if ( ! $event_ids ) {
+			$event_ids = array();
+		}
+		$event_ids[ $event_id ] = true;
+		update_user_meta( $user_id, self::USER_META_KEY, $event_ids );
 	}
 
 	public function remove_attendee( int $event_id, int $user_id ): void {
--- tests/attendee-repository.php
+++ tests/attendee-repository.php
@@ -12,4 +12,31 @@ class Attendee_Repository_Test extends WP_UnitTestCase {
 		parent::setUp();
 		$this->repository = new Attendee_Repository();
 	}
+
+	public function test_add_attendee_invalid_event_id() {
+		$this->expectExceptionMessage( 'invalid event id' );
+		$this->repository->add_attendee( 0, 1 );
+	}
+
+	public function test_add_attendee_invalid_user_id() {
+		$this->expectExceptionMessage( 'invalid user id' );
+		$this->repository->add_attendee( 1, 0 );
+	}
+
+	public function test_add_attendee() {
+		$event1_id = 1;
+		$event2_id = 2;
+		$user_id   = 42;
+
+		$this->repository->add_attendee( $event1_id, $user_id );
+		$this->repository->add_attendee( $event2_id, $user_id );
+
+		$event_ids = get_user_meta( $user_id, Attendee_Repository::USER_META_KEY, true );
+		$this->assertCount( 2, $event_ids );
+		$this->assertTrue( $event_ids[ $event1_id ] );
+		$this->assertTrue( $event_ids[ $event2_id ] );
+
+		$event_ids_another_user = get_user_meta( $user_id + 1, Attendee_Repository::USER_META_KEY, true );
+		$this->assertEmpty( $event_ids_another_user );
+	}
 }
