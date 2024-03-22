29e9829552b4858fbc068ac7613aa97bf944cdf8
Tosin Oguntuyi <tcubed03@gmail.com>
2024-03-22 11:38:47 +0100

    Add snippets below description textarea

--- templates/events-form.php
+++ templates/events-form.php
@@ -43,7 +43,14 @@ gp_tmpl_load( 'events-header', get_defined_vars(), __DIR__ );
 	<div>
 		<label for="event-description">Event Description</label>
 		<textarea id="event-description" name="event_description" rows="4" required><?php echo esc_html( $event_description ); ?></textarea>
-	</div>
+		<ul class="text-snippets">
+			<li>
+				<a href="#">New contributor guidance</a>
+			</li>
+			<li>
+				<a href="#">Generic Instructions</a>
+			</li>	
+		</ul>
 	<div>
 		<label for="event-start">Start Date</label>
 		<input type="datetime-local" id="event-start" name="event_start" value="<?php echo esc_attr( $event_start->format( 'Y-m-d H:i' ) ); ?>" required>
