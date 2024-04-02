e706dc2a68dd6b33ae55fd799f393aa746d89480
Paulo Pinto <paulo.pinto@automattic.com>
2024-04-02 15:35:18 +0100

    Use event repository in event details page

--- includes/routes/event/details.php
+++ includes/routes/event/details.php
@@ -3,10 +3,12 @@
 namespace Wporg\TranslationEvents\Routes\Event;
 
 use DateTime;
+use DateTimeImmutable;
 use DateTimeZone;
 use Exception;
 use GP;
 use Wporg\TranslationEvents\Attendee_Repository;
+use Wporg\TranslationEvents\Event\Event_Repository_Interface;
 use Wporg\TranslationEvents\Routes\Route;
 use Wporg\TranslationEvents\Stats_Calculator;
 use Wporg\TranslationEvents\Translation_Events;
@@ -17,16 +19,22 @@ use Wporg\TranslationEvents\Event_End_Date;
  * Displays the event details page.
  */
 class Details_Route extends Route {
+	private Event_Repository_Interface $event_repository;
 	private Attendee_Repository $attendee_repository;
 
 	public function __construct() {
 		parent::__construct();
-		$this->attendee_repository = new Attendee_Repository();
+		$this->event_repository    = Translation_Events::get_event_repository();
+		$this->attendee_repository = Translation_Events::get_attendee_repository();
 	}
 
 	public function handle( string $event_slug ): void {
 		$user  = wp_get_current_user();
 		$event = get_page_by_path( $event_slug, OBJECT, Translation_Events::CPT );
+		if ( ! $event ) {
+			$this->die_with_404();
+		}
+		$event = $this->event_repository->get_event( $event->ID );
 		if ( ! $event ) {
 			$this->die_with_404();
 		}
@@ -37,22 +45,22 @@ class Details_Route extends Route {
 		 * @param bool $can_crud_event Whether the user can create, edit, or delete an event.
 		 */
 		$can_crud_event = apply_filters( 'gp_translation_events_can_crud_event', GP::$permission->current_user_can( 'admin' ) );
-		if ( 'publish' !== $event->post_status && ! $can_crud_event ) {
+		if ( 'publish' !== $event->status() && ! $can_crud_event ) {
 			$this->die_with_error( esc_html__( 'You are not authorized to view this page.', 'gp-translation-events' ), 403 );
 		}
 
-		$event_id          = $event->ID;
-		$event_title       = $event->post_title;
-		$event_description = $event->post_content;
-		$event_start       = new Event_Start_Date( get_post_meta( $event->ID, '_event_start', true ) ?: '' );
-		$event_end         = new Event_End_Date( get_post_meta( $event->ID, '_event_end', true ) ?: '' );
-		$user_is_attending = $this->attendee_repository->is_attending( $event_id, $user->ID );
+		$event_id          = $event->id();
+		$event_title       = $event->title();
+		$event_description = $event->description();
+		$event_start       = $event->start();
+		$event_end         = $event->end();
+		$user_is_attending = $this->attendee_repository->is_attending( $event->id(), $user->ID );
 
 		$stats_calculator = new Stats_Calculator();
 		try {
-			$event_stats  = $stats_calculator->for_event( $event->ID );
-			$contributors = $stats_calculator->get_contributors( $event_id );
-			$projects     = $stats_calculator->get_projects( $event_id );
+			$event_stats  = $stats_calculator->for_event( $event->id() );
+			$contributors = $stats_calculator->get_contributors( $event->id() );
+			$projects     = $stats_calculator->get_projects( $event->id() );
 		} catch ( Exception $e ) {
 			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 			error_log( $e );
@@ -60,7 +68,7 @@ class Details_Route extends Route {
 		}
 
 		$is_editable_event = true;
-		if ( $event_end->is_in_the_past() || $stats_calculator->event_has_stats( $event_id ) ) {
+		if ( $event_end->is_in_the_past() || $stats_calculator->event_has_stats( $event->id() ) ) {
 			$is_editable_event = false;
 		}
 
