f0dcf4bf8a91c8c4f5cc2b6af3fb60385cfb03c1
Paulo Pinto <paulo.pinto@automattic.com>
2024-04-04 16:31:57 +0100

    Use event repository in Attend_Event_Route

--- includes/routes/user/attend-event.php
+++ includes/routes/user/attend-event.php
@@ -3,7 +3,9 @@
 namespace Wporg\TranslationEvents\Routes\User;
 
 use Wporg\TranslationEvents\Attendee\Attendee_Repository;
+use Wporg\TranslationEvents\Event\Event_Repository_Interface;
 use Wporg\TranslationEvents\Routes\Route;
+use Wporg\TranslationEvents\Translation_Events;
 
 /**
  * Toggle whether the current user is attending an event.
@@ -11,11 +13,13 @@ use Wporg\TranslationEvents\Routes\Route;
  * If the user is currently marked as attending, they will be marked as not attending.
  */
 class Attend_Event_Route extends Route {
+	private Event_Repository_Interface $event_repository;
 	private Attendee_Repository $attendee_repository;
 
 	public function __construct() {
 		parent::__construct();
-		$this->attendee_repository = new Attendee_Repository();
+		$this->event_repository    = Translation_Events::get_event_repository();
+		$this->attendee_repository = Translation_Events::get_attendee_repository();
 	}
 
 	public function handle( int $event_id ): void {
@@ -24,18 +28,18 @@ class Attend_Event_Route extends Route {
 			$this->die_with_error( esc_html__( 'Only logged-in users can attend events', 'gp-translation-events' ), 403 );
 		}
 
-		$event = get_post( $event_id );
+		$event = $this->event_repository->get_event( $event_id );
 		if ( ! $event ) {
 			$this->die_with_404();
 		}
 
-		if ( $this->attendee_repository->is_attending( $event_id, $user->ID ) ) {
-			$this->attendee_repository->remove_attendee( $event_id, $user->ID );
+		if ( $this->attendee_repository->is_attending( $event->id(), $user->ID ) ) {
+			$this->attendee_repository->remove_attendee( $event->id(), $user->ID );
 		} else {
-			$this->attendee_repository->add_attendee( $event_id, $user->ID );
+			$this->attendee_repository->add_attendee( $event->id(), $user->ID );
 		}
 
-		wp_safe_redirect( gp_url( "/events/$event->post_name" ) );
+		wp_safe_redirect( gp_url( "/events/{$event->slug()}" ) );
 		exit;
 	}
 }
