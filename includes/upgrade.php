5310dd56c80d8f092d6f47cfd3f2dc8c5afc489a
Paulo Pinto <paulo.pinto@automattic.com>
2024-04-09 16:08:33 +0100

    Extract the database schema to a function

--- includes/upgrade.php
+++ includes/upgrade.php
@@ -9,35 +9,7 @@ use Wporg\TranslationEvents\Attendee\Attendee;
 class Upgrade {
 	public static function upgrade(): void {
 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-		global $gp_table_prefix;
-
-		// Create actions table.
-		$create_actions_table = "
-		CREATE TABLE `{$gp_table_prefix}event_actions` (
-			`translate_event_actions_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-			`event_id` int(10) NOT NULL COMMENT 'Post_ID of the translation_event post in the wp_posts table',
-			`original_id` int(10) NOT NULL COMMENT 'ID of the translation',
-			`user_id` int(10) NOT NULL COMMENT 'ID of the user who made the action',
-			`action` enum('approve','create','reject','request_changes') NOT NULL COMMENT 'The action that the user made (create, reject, etc)',
-			`locale` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'Locale of the translation',
-			`happened_at` datetime NOT NULL COMMENT 'When the action happened, in UTC',
-		PRIMARY KEY (`translate_event_actions_id`),
-		UNIQUE KEY `event_per_translated_original_per_user` (`event_id`,`locale`,`original_id`,`user_id`)
-		) COMMENT='Tracks translation actions that happened during a translation event'";
-		dbDelta( $create_actions_table );
-
-		// Create attendees table.
-		$create_attendees_table = "
-		CREATE TABLE `{$gp_table_prefix}event_attendees` (
-			`translate_event_attendees_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-			`event_id` int(10) NOT NULL COMMENT 'Post_ID of the translation_event post in the wp_posts table',
-			`user_id` int(10) NOT NULL COMMENT 'ID of the user who is attending the event',
-			`is_host` tinyint(1) default 0 not null comment 'Whether the user is a host of the event',
-		PRIMARY KEY (`translate_event_attendees_id`),
-		UNIQUE KEY `event_per_user` (`event_id`,`user_id`),
-		INDEX `user` (`user_id`)
-		) COMMENT='Attendees of events'";
-		dbDelta( $create_attendees_table );
+		dbDelta( self::get_database_schema_sql() );
 
 		// TODO: Remove this once it has been run in production.
 		try {
@@ -50,6 +22,34 @@ class Upgrade {
 			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 			error_log( $e );
 		}
+	}
+
+	private static function get_database_schema_sql(): string {
+		global $gp_table_prefix;
+
+		return "
+			CREATE TABLE `{$gp_table_prefix}event_actions` (
+				`translate_event_actions_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				`event_id` int(10) NOT NULL COMMENT 'Post_ID of the translation_event post in the wp_posts table',
+				`original_id` int(10) NOT NULL COMMENT 'ID of the translation',
+				`user_id` int(10) NOT NULL COMMENT 'ID of the user who made the action',
+				`action` enum('approve','create','reject','request_changes') NOT NULL COMMENT 'The action that the user made (create, reject, etc)',
+				`locale` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'Locale of the translation',
+				`happened_at` datetime NOT NULL COMMENT 'When the action happened, in UTC',
+			PRIMARY KEY (`translate_event_actions_id`),
+			UNIQUE KEY `event_per_translated_original_per_user` (`event_id`,`locale`,`original_id`,`user_id`)
+			) COMMENT='Tracks translation actions that happened during a translation event';
+
+			CREATE TABLE `{$gp_table_prefix}event_attendees` (
+				`translate_event_attendees_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				`event_id` int(10) NOT NULL COMMENT 'Post_ID of the translation_event post in the wp_posts table',
+				`user_id` int(10) NOT NULL COMMENT 'ID of the user who is attending the event',
+				`is_host` tinyint(1) default 0 not null comment 'Whether the user is a host of the event',
+			PRIMARY KEY (`translate_event_attendees_id`),
+			UNIQUE KEY `event_per_user` (`event_id`,`user_id`),
+			INDEX `user` (`user_id`)
+			) COMMENT='Attendees of events';
+		";
 	}
 
 	/**
