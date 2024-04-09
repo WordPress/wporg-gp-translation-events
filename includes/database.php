0864f7c726d8b4125c412d9b6436849dabbe5b7b
Paulo Pinto <paulo.pinto@automattic.com>
2024-04-09 15:43:05 +0100

    Revert "Set is_host when importing legacy attendees"
    This reverts commit 7e5dd652909e216cf92615cd6aaa5cf611d9fe9a.

--- includes/database.php
+++ includes/database.php
@@ -74,7 +74,6 @@ class Database {
 		foreach ( $events as $event ) {
 			foreach ( $stats_calculator->get_contributors( $event->ID ) as $user ) {
 				$attendee = new Attendee( $event->ID, $user->ID );
-				$attendee->mark_as_host();
 				$attendee_repository->insert_attendee( $attendee );
 			}
 		}
