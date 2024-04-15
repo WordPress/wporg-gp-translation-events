434d684babfdf71df2782841a6d02b776027dd0c
Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>
2024-04-15 13:02:38 +0200

    Update templates/event.php
    Co-authored-by: Tosin <tcubed03@gmail.com>

--- templates/event.php
+++ templates/event.php
@@ -89,7 +89,7 @@ gp_tmpl_load( 'events-header', get_defined_vars(), __DIR__ );
 						<li class="event-attendee">
 							<a href="<?php echo esc_url( get_author_posts_url( $_user->ID ) ); ?>"><?php echo get_avatar( $_user->ID, 48 ); ?></a>
 							<a href="<?php echo esc_url( get_author_posts_url( $_user->ID ) ); ?>"><?php echo esc_html( get_the_author_meta( 'display_name', $_user->ID ) ); ?></a>
-							<?php if ( $stats_calculator->is_first_time_contributor( $event_start, $contributor->ID ) ) : ?>
+							<?php if ( $stats_calculator->is_first_time_contributor( $event_start, $_user->ID ) ) : ?>
 								<span class="first-time-contributor-tada"></span>
 							<?php endif; ?>
 							<?php
