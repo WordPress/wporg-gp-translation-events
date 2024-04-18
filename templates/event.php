8e6bc72ac029721fe7fd62816672fee5cec73d18
Alex Kirk <akirk@users.noreply.github.com>
2024-04-18 13:21:11 +0200

    Show the number of new contributors

--- templates/event.php
+++ templates/event.php
@@ -174,12 +174,25 @@ gp_tmpl_load( 'events-header', get_defined_vars(), __DIR__ );
 				<summary>View stats summary in text </summary>
 				<p class="event-stats-text">
 					<?php
+					$new_contributors = array_filter(
+						$contributors,
+						function ( $contributor ) use ( $stats_calculator, $event_start ) {
+							return $stats_calculator->is_new_translation_contributor( $event_start, $contributor->ID );
+						}
+					);
+
+					$new_contributors_text = '';
+					if ( ! empty( $new_contributors ) ) {
+						$new_contributors_text = sprintf( _n( '(%d new contributor!)', '(%d new contributors!)', count( $new_contributors ), 'gp-translation-events' ), count($new_contributors)	);
+					}
+
 					echo wp_kses(
 						sprintf(
-							// translators: %1$s: Event title, %2$d: Number of contributors, %3$d: Number of languages, %4$s: List of languages, %5$d: Number of strings translated, %6$d: Number of strings reviewed.
-							__( 'At the <strong>%1$s</strong> event, %2$d people contributed in %3$d languages (%4$s), translated %5$d strings and reviewed %6$d strings.', 'gp-translation-events' ),
+							// translators: %1$s: Event title, %2$d: Number of contributors, %3$s: is a parenthesis with potential text "x new contributors", %4$d: Number of languages, %5$s: List of languages, %6$d: Number of strings translated, %7$d: Number of strings reviewed.
+							__( 'At the <strong>%1$s</strong> event, we had %2$d people %3$s who contributed in %4$d languages (%5$s), translated %6$d strings and reviewed %7$d strings.', 'gp-translation-events' ),
 							esc_html( $event_title ),
 							esc_html( $event_stats->totals()->users ),
+							$new_contributors_text,
 							count( $event_stats->rows() ),
 							esc_html(
 								implode(
