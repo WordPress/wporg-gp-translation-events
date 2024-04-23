5293d102b0dcbe2f0002cca2083402a3429df242
Alex Kirk <akirk@users.noreply.github.com>
2024-04-23 17:09:47 +0200

    Update templates/event-translations-header.php
    Co-authored-by: Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>

--- templates/event-translations-header.php
+++ templates/event-translations-header.php
@@ -9,7 +9,7 @@ use Wporg\TranslationEvents\Event\Event;
 
 /* translators: %s: Event title. */
 gp_title( sprintf( __( 'Translation Events - %s' ), esc_html( $event->title() ) ) );
-gp_breadcrumb_translation_events( array( '<a href="' . esc_attr( gp_url( wp_make_link_relative( get_the_permalink( $event->id() ) ) ) ) . '">' . esc_html( $event->title() ) . '</a>', __( 'Translations', 'glotpress' ), $locale ) );
+gp_breadcrumb_translation_events( array( '<a href="' . esc_attr( gp_url( wp_make_link_relative( get_the_permalink( $event->id() ) ) ) ) . '">' . esc_html( $event->title() ) . '</a>', __( 'Translations', 'glotpress' ), $locale->english_name ) );
 gp_enqueue_scripts( array( 'gp-editor', 'gp-translations-page' ) );
 wp_localize_script(
 	'gp-translations-page',
