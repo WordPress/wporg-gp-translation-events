adb7317ebdc006b20551c37ddfb087b8aad43fa9
Tosin Oguntuyi <tcubed03@gmail.com>
2024-04-26 08:45:34 +0100

    Add table view

--- includes/routes/attendee/list.php
+++ includes/routes/attendee/list.php
@@ -2,6 +2,7 @@
 
 namespace Wporg\TranslationEvents\Routes\Attendee;
 
+use Wporg\TranslationEvents\Attendee\Attendee_Repository;
 use Wporg\TranslationEvents\Routes\Route;
 
 
@@ -9,7 +10,12 @@ use Wporg\TranslationEvents\Routes\Route;
  * Displays the event list page.
  */
 class List_Route extends Route {
-	public function handle(): void {
+	private Attendee_Repository $attendee_repository;
+
+
+	public function handle( int $event_id ): void {
+		$this->attendee_repository = new Attendee_Repository();
+		$attendees                 = $this->attendee_repository->get_attendees( $event_id );
 		$this->tmpl( 'events-attendees', get_defined_vars() );
 	}
 }
--- templates/events-attendees.php
+++ templates/events-attendees.php
@@ -10,7 +10,40 @@ gp_breadcrumb_translation_events();
 gp_tmpl_header();
 $event_page_title = __( 'Manage Attendees', 'gp-translation-events' );
 gp_tmpl_load( 'events-header', get_defined_vars(), __DIR__ );
-?>
 
+
+
+?>
+<div class="event-page-wrapper">
+<div class="event-details-stats">
+				<h2><?php esc_html_e( 'Stats', 'gp-translation-events' ); ?></h2>
+				<table>
+	<thead>
+		<tr>
+			<th scope="col"><?php esc_html_e( 'Name', 'gp-translation-events' ); ?></th>
+			<th><?php esc_html_e( 'Role', 'gp-translation-events' ); ?></th>
+			<th><?php esc_html_e( 'Action', 'gp-translation-events' ); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ( $attendees as $attendee ) : ?>
+			<tr>
+				<td>
+					<a href="<?php echo esc_url( get_author_posts_url( $attendee->user_id() ) ); ?>" class="avatar"><?php echo get_avatar( $attendee->user_id(), 48 ); ?></a>
+					<a href="<?php echo esc_url( get_author_posts_url( $attendee->user_id() ) ); ?>" class="name"><?php echo esc_html( get_the_author_meta( 'display_name', $attendee->user_id() ) ); ?></a>
+				</td>
+				<td>
+					<?php if ( $attendee->is_host() ) : ?>
+						<span>Host</span>
+						<?php endif; ?>
+				</td>
+				<td>
+					<a href="">Remove</a>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+</div>
 <?php
 gp_tmpl_footer();
