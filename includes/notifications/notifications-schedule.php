27dd511e854808e63b1401b9a7287bbf8dda3a75
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2024-04-26 12:44:49 +0200

    Check if the event exists before enqueing the email notifications

--- includes/notifications/notifications-schedule.php
+++ includes/notifications/notifications-schedule.php
@@ -25,16 +25,20 @@ class Notifications_Schedule {
 	 * @return void
 	 */
 	public function schedule_emails( int $post_id ) {
+		$event = $this->event_repository->get_event( $post_id );
+		if ( ! $event ) {
+			return;
+		}
 		$args = array(
 			'post_id' => $post_id,
 		);
-
 		$this->delete_scheduled_emails( $post_id );
-		$event                 = $this->event_repository->get_event( $post_id );
-		$new_next_1h_schedule  = $event->start()->getTimestamp() - HOUR_IN_SECONDS;
-		$new_next_24h_schedule = $event->start()->getTimestamp() - 24 * HOUR_IN_SECONDS;
-		wp_schedule_single_event( $new_next_1h_schedule, 'wporg_gp_translation_events_email_notifications_1h', $args );
-		wp_schedule_single_event( $new_next_24h_schedule, 'wporg_gp_translation_events_email_notifications_24h', $args );
+		if ( 'publish' === get_post_status( $post_id ) ) {
+			$new_next_1h_schedule  = $event->start()->getTimestamp() - HOUR_IN_SECONDS;
+			$new_next_24h_schedule = $event->start()->getTimestamp() - 24 * HOUR_IN_SECONDS;
+			wp_schedule_single_event( $new_next_1h_schedule, 'wporg_gp_translation_events_email_notifications_1h', $args );
+			wp_schedule_single_event( $new_next_24h_schedule, 'wporg_gp_translation_events_email_notifications_24h', $args );
+		}
 	}
 
 	/**
