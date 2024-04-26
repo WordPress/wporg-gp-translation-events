17bcb96d212eac7ed757aac597aab13c04e613f5
Tosin Oguntuyi <tcubed03@gmail.com>
2024-04-26 17:26:42 +0100

    Add filter

--- includes/routes/attendee/list.php
+++ includes/routes/attendee/list.php
@@ -4,6 +4,8 @@ namespace Wporg\TranslationEvents\Routes\Attendee;
 
 use Wporg\TranslationEvents\Attendee\Attendee_Repository;
 use Wporg\TranslationEvents\Routes\Route;
+use Wporg\TranslationEvents\Attendee\Attendee;
+use Wporg\TranslationEvents\Stats\Stats_Calculator;
 
 
 /**
@@ -11,11 +13,53 @@ use Wporg\TranslationEvents\Routes\Route;
  */
 class List_Route extends Route {
 	private Attendee_Repository $attendee_repository;
+	private Stats_Calculator $stats_calculator;
 
+
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->attendee_repository = new Attendee_Repository();
+		$this->stats_calculator    = new Stats_Calculator();
+	}
 
 	public function handle( int $event_id ): void {
-		$this->attendee_repository = new Attendee_Repository();
-		$attendees                 = $this->attendee_repository->get_attendees( $event_id );
+		$attendees = array();
+		if ( gp_get( 'filter' ) && 'hosts' !== gp_get( 'filter' ) ) {
+			$filtered_result = $this->filter_attendees( gp_get( 'filter' ), $event_id );
+			if ( $filtered_result ) {
+				$attendees = array();
+				foreach ( $filtered_result as $row ) {
+					$attendee = $this->attendee_repository->get_attendee( $event_id, intval( $row->ID ) );
+					if ( '1' === $attendee->is_host() ) {
+						$attendee->mark_as_host();
+					}
+					$attendees[] = $attendee;
+				}
+			}
+		} elseif ( gp_get( 'filter' ) && 'hosts' === gp_get( 'filter' ) ) {
+			$attendees = $this->attendee_repository->get_hosts( $event_id );
+		} else {
+			$attendees = $this->attendee_repository->get_attendees( $event_id );
+		}
+
+		$url = gp_url( 'events/' . $event_id . '/attendees' );
 		$this->tmpl( 'events-attendees', get_defined_vars() );
+	}
+
+	private function filter_attendees( $filter, $event_id ) {
+		$this->attendee_repository = new Attendee_Repository();
+		switch ( $filter ) {
+			case 'hosts':
+				return $this->attendee_repository->get_hosts( $event_id );
+			case 'contributors':
+				return $this->stats_calculator->get_contributors( $event_id );
+			case 'new_contributors':
+				// todo.
+				return array();
+			case 'non-contributors':
+				$this->stats_calculator->get_attendees_not_contributing( $event_id );
+		}
 	}
 }
--- templates/events-attendees.php
+++ templates/events-attendees.php
@@ -15,34 +15,45 @@ gp_tmpl_load( 'events-header', get_defined_vars(), __DIR__ );
 
 ?>
 <div class="event-page-wrapper">
-<div class="event-details-stats">
-<table>
-	<thead>
-		<tr>
-			<th scope="col"><?php esc_html_e( 'Name', 'gp-translation-events' ); ?></th>
-			<th><?php esc_html_e( 'Role', 'gp-translation-events' ); ?></th>
-			<th><?php esc_html_e( 'Action', 'gp-translation-events' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ( $attendees as $attendee ) : ?>
+	<div class="event-details-stats">
+	<ul class="event-attendees-filter">
+		<li><a href="<?php echo esc_url( $url ); ?>"><?php esc_html_e( 'All attendees', 'gp-translation-events' ); ?></a></li>
+		<li><a href="?filter=hosts"><?php esc_html_e( 'Hosts', 'gp-translation-events' ); ?></a></li>
+		<li><a href="?filter=contributors"><?php esc_html_e( 'Contributors', 'gp-translation-events' ); ?></a></li>
+		<li><a href="?filter=new-contributors"><?php esc_html_e( 'New Translation Contributors', 'gp-translation-events' ); ?></a></li>
+		<li><a href="?filter=non-contributors"><?php esc_html_e( 'Non-Contributors', 'gp-translation-events' ); ?></a></li>
+	</ul>
+<?php if ( ! empty( $attendees ) ) : ?>
+	<table>
+		<thead>
 			<tr>
-				<td>
-					<a class="attendee-avatar" href="<?php echo esc_url( get_author_posts_url( $attendee->user_id() ) ); ?>" class="avatar"><?php echo get_avatar( $attendee->user_id(), 48 ); ?></a>
-					<a href="<?php echo esc_url( get_author_posts_url( $attendee->user_id() ) ); ?>" class="name"><?php echo esc_html( get_the_author_meta( 'display_name', $attendee->user_id() ) ); ?></a>
-				</td>
-				<td>
-					<?php if ( $attendee->is_host() ) : ?>
-						<span>Host</span>
-						<?php endif; ?>
-				</td>
-				<td>
-					<a href="">Remove</a>
-				</td>
+				<th scope="col"><?php esc_html_e( 'Name', 'gp-translation-events' ); ?></th>
+				<th><?php esc_html_e( 'Role', 'gp-translation-events' ); ?></th>
+				<th><?php esc_html_e( 'Action', 'gp-translation-events' ); ?></th>
 			</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
-</div>
+		</thead>
+		<tbody>
+			<?php foreach ( $attendees as $attendee ) : ?>
+				<tr>
+					<td>
+						<a class="attendee-avatar" href="<?php echo esc_url( get_author_posts_url( $attendee->user_id() ) ); ?>" class="avatar"><?php echo get_avatar( $attendee->user_id(), 48 ); ?></a>
+						<a href="<?php echo esc_url( get_author_posts_url( $attendee->user_id() ) ); ?>" class="name"><?php echo esc_html( get_the_author_meta( 'display_name', $attendee->user_id() ) ); ?></a>
+					</td>
+					<td>
+						<?php if ( $attendee->is_host() ) : ?>
+							<span>Host</span>
+							<?php endif; ?>
+					</td>
+					<td>
+						<a href="">Remove</a>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+	<?php else : ?>
+		<p><?php esc_html_e( 'No attendees found.', 'gp-translation-events' ); ?></p>
+	</div>
+<?php endif; ?>
 <?php
 gp_tmpl_footer();
