17d2575ed1c04b23ffe5f9cc2253aab92b32506d
Tosin Oguntuyi <tcubed03@gmail.com>
2024-04-29 13:59:47 +0100

    Fix typo

--- includes/routes/attendee/list.php
+++ includes/routes/attendee/list.php
@@ -31,7 +31,7 @@ class List_Route extends Route {
 			wp_safe_redirect( wp_login_url( home_url( $wp->request ) ) );
 			exit;
 		}
-		if ( ! current_user_can( 'edit_translation_event', $event_id() ) ) {
+		if ( ! current_user_can( 'edit_translation_event', $event_id ) ) {
 			$this->die_with_error( esc_html__( 'You do not have permission to edit this event.', 'gp-translation-events' ), 403 );
 		}
 		$attendees = array();
