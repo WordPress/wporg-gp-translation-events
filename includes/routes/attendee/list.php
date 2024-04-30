c26a92b0a9a310a18b89786a587d88f1645d10c5
Tosin Oguntuyi <tcubed03@gmail.com>
2024-04-30 14:28:42 +0100

    Remove comment

--- includes/routes/attendee/list.php
+++ includes/routes/attendee/list.php
@@ -26,9 +26,9 @@ class List_Route extends Route {
 			wp_safe_redirect( wp_login_url( home_url( $wp->request ) ) );
 			exit;
 		}
-		// if ( ! current_user_can( 'edit_translation_event', $event_id ) ) {
-		// 	$this->die_with_error( esc_html__( 'You do not have permission to edit this event.', 'gp-translation-events' ), 403 );
-		// }
+		if ( ! current_user_can( 'edit_translation_event', $event_id ) ) {
+			$this->die_with_error( esc_html__( 'You do not have permission to edit this event.', 'gp-translation-events' ), 403 );
+		}
 		$attendees = array();
 
 		if ( gp_get( 'filter' ) && 'hosts' == gp_get( 'filter' ) ) {
