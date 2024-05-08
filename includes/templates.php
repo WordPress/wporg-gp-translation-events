e55e38de340f9b6af83b3a50aa29fd1642a65fab
Paulo Pinto <paulo.pinto@automattic.com>
2024-05-08 13:44:24 +0100

    Simplify Templates class

--- includes/templates.php
+++ includes/templates.php
@@ -3,29 +3,15 @@
 namespace Wporg\TranslationEvents;
 
 class Templates {
-	private string $base_path;
-
-	public function __construct() {
-		$this->base_path = __DIR__ . '/../templates/';
-	}
-
-	public static function get_instance(): Templates {
-		static $instance = null;
-		if ( null === $instance ) {
-			$instance = new self();
-		}
-		return $instance;
-	}
-
-	public function render( string $template, array $data = array() ) {
-		gp_tmpl_load( $template, $data, $this->base_path );
+	public static function render( string $template, array $data = array() ) {
+		gp_tmpl_load( $template, $data, __DIR__ . '/../templates/' );
 	}
 
 	public static function header( array $data ) {
-		self::get_instance()->render( 'header', $data );
+		self::render( 'header', $data );
 	}
 
 	public static function footer( array $data = array() ) {
-		self::get_instance()->render( 'footer', $data );
+		self::render( 'footer', $data );
 	}
 }
