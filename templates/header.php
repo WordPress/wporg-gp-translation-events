d0c2ac1bb47c7dfe27991792afa1d532f3a373f8
Paulo Pinto <paulo.pinto@automattic.com>
2024-05-08 13:35:00 +0100

    Extract logic specific to the event details page out of the header

--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,11 @@
 <?php
 
-use Wporg\TranslationEvents\Attendee\Attendee;
-use Wporg\TranslationEvents\Event\Event;
 use Wporg\TranslationEvents\Urls;
 
 /** @var string $html_title */
 /** @var string|callable $page_title */
+/** @var ?callable $sub_head */
 /** @var string[] $breadcrumbs */
-/** @var WP_User $user */
-
-/** @var Event $event */
-/** @var Attendee[] $hosts */
 
 gp_title( $html_title );
 gp_breadcrumb_translation_events( $breadcrumbs );
@@ -38,37 +33,9 @@ gp_tmpl_header();
 		<?php endif; ?>
 	</ul>
 
-	<?php if ( isset( $event ) && ! isset( $event_form_name ) ) : ?>
+	<?php if ( is_callable( $sub_head ) ) : ?>
 		<p class="event-sub-head">
-			<span class="event-host">
-				<?php
-				if ( isset( $hosts ) ) :
-					if ( count( $hosts ) > 0 ) :
-						if ( 1 === count( $hosts ) ) :
-							esc_html_e( 'Host:', 'gp-translation-events' );
-						else :
-							esc_html_e( 'Hosts:', 'gp-translation-events' );
-						endif;
-					else :
-						esc_html_e( 'Created by:', 'gp-translation-events' );
-						?>
-						&nbsp;<a href="<?php echo esc_attr( get_author_posts_url( $user->ID ) ); ?>"><?php echo esc_html( get_the_author_meta( 'display_name', $user->ID ) ); ?></a>
-					<?php endif; ?>
-					<?php foreach ( $hosts as $host ) : ?>
-						&nbsp;<a href="<?php echo esc_attr( get_author_posts_url( $host->user_id() ) ); ?>"><?php echo esc_html( get_the_author_meta( 'display_name', $host->user_id() ) ); ?></a>
-						<?php if ( end( $hosts ) !== $host ) : ?>
-						,
-						<?php else : ?>
-						.
-						<?php endif; ?>
-					<?php endforeach; ?>
-				<?php endif; ?>
-			</span>
-			<?php if ( current_user_can( 'edit_translation_event', $event->id() ) ) : ?>
-				<a class="event-page-edit-link" href="<?php echo esc_url( Urls::event_edit( $event->id() ) ); ?>"><span class="dashicons dashicons-edit"></span><?php esc_html_e( 'Edit event', 'gp-translation-events' ); ?></a>
-			<?php elseif ( current_user_can( 'edit_translation_event_attendees', $event->id() ) ) : ?>
-				<a class="event-page-attendees-link" href="<?php echo esc_url( Urls::event_attendees( $event->id() ) ); ?>"><?php esc_html_e( 'Manage attendees', 'gp-translation-events' ); ?></a>
-			<?php endif ?>
+			<?php $sub_head(); ?>
 		</p>
 	<?php endif; ?>
 </div>
