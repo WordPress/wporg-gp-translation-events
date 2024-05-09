6dab1780347b5a41335dd01c93a67cd950e32f60
Paulo Pinto <paulo.pinto@automattic.com>
2024-05-09 13:02:20 +0100

    Show edit button

--- templates/partials/event-list.php
+++ templates/partials/event-list.php
@@ -46,6 +46,9 @@ $print_time = function ( $time ) use ( $date_format, $relative_time ): void {
 			<?php if ( $event->is_draft() ) : ?>
 				<span class="event-label-draft">Draft</span>
 			<?php endif; ?>
+			<?php if ( current_user_can( 'edit_translation_event', $event->id() ) ) : ?>
+				<a href="<?php echo esc_url( Urls::event_edit( $event->id() ) ); ?>" class="button is-small action edit">Edit</a>
+			<?php endif; ?>
 			<?php if ( $show_start ) : ?>
 				<?php if ( $event->start()->is_in_the_past() ) : ?>
 					<span class="event-list-date">started <?php $print_time( $event->start() ); ?></span>
