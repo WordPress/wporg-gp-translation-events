a822359e6b65fba585e069bad9c59f0e33697354
Tosin Oguntuyi <tcubed03@gmail.com>
2024-05-14 08:47:23 +0100

    Remove unused imports

--- tests/stats/stats-calculator.php
+++ tests/stats/stats-calculator.php
@@ -4,8 +4,6 @@ namespace Wporg\Tests\Stats;
 
 use GP_UnitTestCase;
 use Wporg\TranslationEvents\Stats\Stats_Calculator;
-use Wporg\TranslationEvents\Event\Event_Repository_Interface;
-use Wporg\TranslationEvents\Translation_Events;
 use Wporg\TranslationEvents\Tests\Event_Factory;
 use Wporg\TranslationEvents\Tests\Stats_Factory;
 
@@ -13,15 +11,13 @@ class Stats_Calculator_Test extends GP_UnitTestCase {
 	private Event_Factory $event_factory;
 	private Stats_Factory $stats_factory;
 	private Stats_Calculator $calculator;
-	private Event_Repository_Interface $event_repository;
 
 
 	public function setUp(): void {
 		parent::setUp();
-		$this->event_factory    = new Event_Factory();
-		$this->stats_factory    = new Stats_Factory();
-		$this->calculator       = new Stats_Calculator();
-		$this->event_repository = Translation_Events::get_event_repository();
+		$this->event_factory = new Event_Factory();
+		$this->stats_factory = new Stats_Factory();
+		$this->calculator    = new Stats_Calculator();
 	}
 
 	public function test_tells_that_event_has_no_stats() {
