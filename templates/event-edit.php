b5acab05ecd417ab87e24afbb92de04f6d077207
Paulo Pinto <paulo.pinto@automattic.com>
2024-06-03 14:46:04 +0100

    Make events-form dedicated to the event edit page

--- templates/event-edit.php
+++ templates/event-edit.php
@@ -9,14 +9,13 @@ use Wporg\TranslationEvents\Event_Text_Snippet;
 use Wporg\TranslationEvents\Templates;
 use Wporg\TranslationEvents\Urls;
 
-/** @var bool $is_create_form */
 /** @var Event $event */
 
-$page_title = $is_create_form ? 'Create Event' : 'Edit Event';
+$page_title = __( 'Edit Event', 'gp-translation-events' );
 
 Templates::header(
 	array(
-		'html_title'  => __( 'Translation Events' ) . ' - ' . esc_html( $page_title . ' - ' . $event->title() ),
+		'html_title'  => __( 'Translation Events', 'gp-translation-events' ) . ' - ' . esc_html( $page_title . ' - ' . $event->title() ),
 		'page_title'  => $page_title,
 		'breadcrumbs' => array( esc_html( $page_title ) ),
 	),
@@ -24,13 +23,12 @@ Templates::header(
 ?>
 
 <div class="event-page-wrapper">
+	<?php $is_create_form = false; ?>
 	<?php Templates::part( 'event-form', compact( 'is_create_form', 'event' ) ); ?>
 </div>
 
-<?php if ( $event->id() ) : ?>
-	<div class="event-edit-right">
-		<a class="manage-attendees-btn button is-primary" href="<?php echo esc_url( Urls::event_attendees( $event->id() ) ); ?>"><?php esc_html_e( 'Manage Attendees', 'gp-translation-events' ); ?></a>
-	</div>
-<?php endif; ?>
+<div class="event-edit-right">
+	<a class="manage-attendees-btn button is-primary" href="<?php echo esc_url( Urls::event_attendees( $event->id() ) ); ?>"><?php esc_html_e( 'Manage Attendees', 'gp-translation-events' ); ?></a>
+</div>
 
 <?php Templates::footer(); ?>
