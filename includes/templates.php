de21506a446beefa82e9e755586ed36490260962
Paulo Pinto <paulo.pinto@automattic.com>
2024-06-03 16:34:43 +0100

    Add function for enabling new design for the current request

--- includes/templates.php
+++ includes/templates.php
@@ -3,6 +3,22 @@
 namespace Wporg\TranslationEvents;
 
 class Templates {
+	private static bool $use_new_design = false;
+
+	public static function use_new_design( bool $also_in_production = false ): void {
+		if ( $also_in_production ) {
+			// If it's enabled for production, it's also enabled for development, so it's always enabled.
+			self::$use_new_design = true;
+		} else {
+			// Only enable if new design has been explicitly enabled.
+			self::$use_new_design = defined( 'TRANSLATION_EVENTS_NEW_DESIGN' ) && TRANSLATION_EVENTS_NEW_DESIGN;
+		}
+
+		if ( self::$use_new_design ) {
+			// TODO.
+		}
+	}
+
 	public static function render( string $template, array $data = array() ) {
 		gp_tmpl_load( $template, $data, __DIR__ . '/../templates/' );
 	}
