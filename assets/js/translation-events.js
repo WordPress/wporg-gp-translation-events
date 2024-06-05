ff0c3180f512e76f20d684b54c208a209d72ae9f
Alex Kirk <akirk@users.noreply.github.com>
2024-06-05 16:08:24 +0200

    lint

--- assets/js/translation-events.js
+++ assets/js/translation-events.js
@@ -285,7 +285,7 @@
 						const seconds    = Math.floor( diff / 1000 );
 						const minutes    = Math.floor( seconds / 60 );
 						const hours      = Math.floor( minutes / 60 );
-						let days       = Math.floor( hours / 24 );
+						let days         = Math.floor( hours / 24 );
 						const weeks      = Math.floor( days / 7 );
 						const months     = Math.floor( days / 30 );
 						const years      = Math.floor( days / 365.25 );
