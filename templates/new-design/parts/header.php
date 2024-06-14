ce29af24a5951df62eb6910e90e5ec27fedd2918
Paulo Pinto <paulo.pinto@automattic.com>
2024-06-14 15:40:57 +0100

    Merge site-header into header

--- templates/new-design/parts/header.php
+++ templates/new-design/parts/header.php
@@ -4,13 +4,10 @@ namespace Wporg\TranslationEvents\Templates\NewDesign\Parts;
 use Wporg\TranslationEvents\Templates;
 use Wporg\TranslationEvents\Urls;
 
-/** @var string $html_title */
-/** @var string|callable $page_title */
 /** @var string $url */
-/** @var string $image_url */
+/** @var string $html_title */
 /** @var string $html_description */
-/** @var ?callable $sub_head */
-/** @var ?string[] $breadcrumbs */
+/** @var string $image_url */
 
 $html_title       = $html_title ?? esc_html__( 'Translation Events', 'gp-translation-events' );
 $url              = $url ?? Urls::events_home();
@@ -18,5 +15,89 @@ $html_description = $html_description ?? esc_html__( 'WordPress Translation Even
 $image_url        = $image_url ?? Urls::event_default_image();
 
 gp_title( $html_title );
-Templates::part( 'site-header', get_defined_vars() );
+
+Templates::block( 'wporg/global-header' );
+add_social_tags( $html_title, $url, $html_description, $image_url );
+add_filter( 'wporg_block_navigation_menus', __NAMESPACE__ . '\add_site_navigation_menus' );
+
+Templates::pattern( 'wporg-translate-events/local-nav' );
 Templates::block( 'wporg/site-breadcrumbs' );
+
+/**
+ * Provide a list with a navigation menu.
+ *
+ * @return array
+ */
+function add_site_navigation_menus(): array {
+	return array(
+		'site-header-menu' => array(
+			array(
+				'label' => esc_html__( 'Events', 'wporg-plugins' ),
+				'url'   => 'https://translate.wordpress.org/events/',
+			),
+			array(
+				'label' => esc_html__( 'Team', 'wporg-plugins' ),
+				'url'   => 'https://make.wordpress.org/polyglots/teams/',
+			),
+			array(
+				'label' => esc_html__( 'Requests', 'wporg-plugins' ),
+				'url'   => 'https://make.wordpress.org/polyglots/?resolved=unresolved',
+			),
+			array(
+				'label' => esc_html__( 'Weekly Chats', 'wporg-plugins' ),
+				'url'   => 'https://make.wordpress.org/polyglots/category/weekly-chats/',
+			),
+			array(
+				'label' => esc_html__( 'Translate', 'wporg-plugins' ),
+				'url'   => 'https://translate.wordpress.org/',
+			),
+			array(
+				'label' => esc_html__( 'Handbook', 'wporg-plugins' ),
+				'url'   => 'https://make.wordpress.org/polyglots/handbook/',
+			),
+		),
+	);
+}
+
+/**
+ * Add social tags to the head of the page.
+ *
+ * @param string $html_title       The title of the page.
+ * @param string $url              The URL of the page.
+ * @param string $html_description The description of the page.
+ * @param string $image_url        The URL of the image to use.
+ *
+ * @return void
+ */
+function add_social_tags( string $html_title, string $url, string $html_description, string $image_url ) {
+	$meta_tags = array(
+		'name'     => array(
+			'twitter:card'        => 'summary',
+			'twitter:site'        => '@WordPress',
+			'twitter:title'       => esc_attr( $html_title ),
+			'twitter:description' => esc_attr( $html_description ),
+			'twitter:creator'     => '@WordPress',
+			'twitter:image'       => esc_url( $image_url ),
+			'twitter:image:alt'   => esc_attr( $html_title ),
+		),
+		'property' => array(
+			'og:url'              => esc_url( $url ),
+			'og:title'            => esc_attr( $html_title ),
+			'og:description'      => esc_attr( $html_description ),
+			'og:site_name'        => esc_attr( get_bloginfo() ),
+			'og:image:url'        => esc_url( $image_url ),
+			'og:image:secure_url' => esc_url( $image_url ),
+			'og:image:type'       => 'image/png',
+			'og:image:width'      => '1200',
+			'og:image:height'     => '675',
+			'og:image:alt'        => esc_attr( $html_title ),
+		),
+	);
+
+	foreach ( $meta_tags as $name => $content ) {
+		foreach ( $content as $key => $value ) {
+			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo '<meta ' . esc_attr( $name ) . '="' . esc_attr( $key ) . '" content="' . esc_attr( $value ) . '" />' . "\n";
+		}
+	}
+}
