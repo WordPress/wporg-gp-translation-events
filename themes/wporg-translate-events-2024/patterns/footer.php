7ead20adeeb78fff8e9ca76d53752117f54f26dc
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-02 15:06:47 +0100

    Move footer to pattern

--- themes/wporg-translate-events-2024/patterns/footer.php
+++ themes/wporg-translate-events-2024/patterns/footer.php
@@ -4,4 +4,9 @@
  * Slug: wporg-translation-events-2024/footer
  */
 namespace Wporg\TranslationEvents\Patterns; ?>
-<span>hello from footer</span>
+
+	<!-- wp:wporg/global-footer /-->
+	</div> <?php // End of wp-site-blocks. ?>
+	<?php wp_footer(); ?>
+</body>
+</html>
