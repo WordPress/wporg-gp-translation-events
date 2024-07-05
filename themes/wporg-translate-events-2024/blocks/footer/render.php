f19066e7284a69eb573cd96aa10d09ce1b87fb0e
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-05 12:16:44 +0100

    Fix linting

--- themes/wporg-translate-events-2024/blocks/footer/render.php
+++ themes/wporg-translate-events-2024/blocks/footer/render.php
@@ -1,7 +1,8 @@
 <?php namespace Wporg\TranslationEvents\Theme_2024; ?>
 
-		</div> <?php // Close wp-site-blocks div, opened in block. ?>
-		<?php echo do_blocks( '<!-- wp:wporg/global-footer /-->'); ?>
+		</div> <?php // Close wp-site-blocks div, opened in header block. ?>
+		<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+		<?php echo do_blocks( '<!-- wp:wporg/global-footer /-->' ); ?>
 		<?php wp_footer(); ?>
 	</body>
 </html>
