5187a4503f78cf5886d7a9a7c0db4032b040d824
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-05 14:38:45 +0100

    Use do_blocks directly

--- themes/wporg-translate-events-2024/blocks/page/render.php
+++ themes/wporg-translate-events-2024/blocks/page/render.php
@@ -6,12 +6,12 @@ $page_name       = $attributes['name'];
 $page_attributes = $attributes['data'];
 $page_block      = "wporg-translate-events-2024/events-pages-$page_name";
 
-$page_content = Templates::block( $page_block, $page_attributes );
+$page_content = do_blocks( "<!-- wp:$page_block " . wp_json_encode( $page_attributes ) . ' /-->' );
 
 // The header and footer blocks must be rendered last, because other blocks may register styles or scripts,
 // or modify the page title, or add breadcrumbs.
-$header_content = Templates::block( 'wporg-translate-events-2024/header' );
-$footer_content = Templates::block( 'wporg-translate-events-2024/footer' );
+$header_content = do_blocks( '<!-- wp:wporg-translate-events-2024/header /-->' );
+$footer_content = do_blocks( '<!-- wp:wporg-translate-events-2024/footer /-->' );
 
 // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 echo $header_content . $page_content . $footer_content;
