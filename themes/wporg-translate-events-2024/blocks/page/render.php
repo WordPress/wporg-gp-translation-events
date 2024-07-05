f97cfe966031ce3c5d77b9903bb9243233976b8d
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-05 15:28:22 +0100

    Split page content from header

--- themes/wporg-translate-events-2024/blocks/page/render.php
+++ themes/wporg-translate-events-2024/blocks/page/render.php
@@ -13,8 +13,9 @@ $page_content = do_blocks( "<!-- wp:$page_block " . wp_json_encode( $page_attrib
 $header_content = do_blocks( '<!-- wp:wporg-translate-events-2024/header /-->' );
 
 // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-echo $header_content . $page_content;
+echo $header_content;
 ?>
+			<?php echo $page_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		</div> <?php // Close wp-site-blocks div, opened in header block. ?>
 		<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		<?php echo do_blocks( '<!-- wp:wporg/global-footer /-->' ); ?>
