c40b75b29555706f5110ca4d3af3c53f513114ea
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-05 12:13:12 +0100

    Remove pattern loading logic

--- themes/wporg-translate-events-2024/functions.php
+++ themes/wporg-translate-events-2024/functions.php
@@ -2,7 +2,6 @@
 
 namespace Wporg\TranslationEvents\Theme_2024;
 
-use WP_Block_Patterns_Registry;
 use Wporg\TranslationEvents\Urls;
 
 require_once __DIR__ . '/autoload.php';
@@ -17,7 +16,6 @@ add_action(
 add_action(
 	'wporg_translate_events_theme_init',
 	function (): void {
-		register_patterns();
 		register_blocks();
 
 		add_action(
@@ -108,93 +106,6 @@ function register_blocks(): void {
 	include_once __DIR__ . '/blocks/footer/index.php';
 	include_once __DIR__ . '/blocks/page-title/index.php';
 	include_once __DIR__ . '/blocks/pages/events/my-events/index.php';
-}
-
-function register_patterns(): void {
-	// This code has been copied from:
-	// https://github.com/WordPress/wordcamp.org/blob/production/public_html/wp-content/mu-plugins/blocks/patterns.php.
-
-	$files = glob( __DIR__ . '/patterns/*.php' );
-	if ( ! is_array( $files ) ) {
-		return;
-	}
-
-	$headers = array(
-		'title' => 'Title',
-		'slug'  => 'Slug',
-	);
-
-	foreach ( $files as $file ) {
-		$pattern_data = get_file_data( $file, $headers );
-
-		if ( empty( $pattern_data['slug'] ) ) {
-			_doing_it_wrong(
-				'register_patterns',
-				esc_html(
-					sprintf(
-					/* translators: %s: file name. */
-						__( 'Could not register file "%s" as a block pattern ("Slug" field missing)', 'wporg-translate-events-2024' ),
-						$file
-					)
-				),
-				''
-			);
-			continue;
-		}
-
-		if ( ! preg_match( '/^[A-z0-9\/_-]+$/', $pattern_data['slug'] ) ) {
-			_doing_it_wrong(
-				'register_patterns',
-				esc_html(
-					sprintf(
-					/* translators: %1s: file name; %2s: slug value found. */
-						__( 'Could not register file "%1$s" as a block pattern (invalid slug "%2$s")', 'wporg-translate-events-2024' ),
-						$file,
-						$pattern_data['slug']
-					)
-				),
-				''
-			);
-		}
-
-		if ( WP_Block_Patterns_Registry::get_instance()->is_registered( $pattern_data['slug'] ) ) {
-			continue;
-		}
-
-		// Title is a required property.
-		if ( ! $pattern_data['title'] ) {
-			_doing_it_wrong(
-				'register_patterns',
-				esc_html(
-					sprintf(
-					/* translators: %1s: file name; %2s: slug value found. */
-						__( 'Could not register file "%s" as a block pattern ("Title" field missing)', 'wporg-translate-events-2024' ),
-						$file
-					)
-				),
-				''
-			);
-			continue;
-		}
-
-		// phpcs:disable WordPress.WP.I18n.LowLevelTranslationFunction
-		// phpcs:disable WordPress.WP.I18n.NonSingularStringLiteralText
-		$pattern_data['title'] = translate_with_gettext_context( $pattern_data['title'], 'Pattern title', 'wporg-translate-events-2024' );
-		if ( ! empty( $pattern_data['description'] ) ) {
-			$pattern_data['description'] = translate_with_gettext_context( $pattern_data['description'], 'Pattern description', 'wporg-translate-events-2024' );
-		}
-		// phpcs:enable
-
-		// The actual pattern content is the output of the file.
-		ob_start();
-		include_once $file;
-		$pattern_data['content'] = ob_get_clean();
-		if ( ! $pattern_data['content'] ) {
-			continue;
-		}
-
-		register_block_pattern( $pattern_data['slug'], $pattern_data );
-	}
 }
 
 /**
