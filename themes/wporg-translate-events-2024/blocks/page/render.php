b7346ea30c78c43eb302e379ce3d1b113b54a03e
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-05 15:31:33 +0100

    Move header to page block:

--- themes/wporg-translate-events-2024/blocks/page/render.php
+++ themes/wporg-translate-events-2024/blocks/page/render.php
@@ -8,13 +8,44 @@ $page_block      = "wporg-translate-events-2024/pages-$page_name";
 
 $page_content = do_blocks( "<!-- wp:$page_block " . wp_json_encode( $page_attributes ) . ' /-->' );
 
-// The header and footer blocks must be rendered last, because other blocks may register styles or scripts,
-// or modify the page title, or add breadcrumbs.
-$header_content = do_blocks( '<!-- wp:wporg-translate-events-2024/header /-->' );
+// The header content must be rendered before the call to wp_head() below,
+// so that styles and scripts of the referenced blocks are registered.
+$header = do_blocks(
+	<<<BLOCKS
+	<!-- wp:wporg/global-header {"style":{"border":{"bottom":{"color":"var:preset|color|white-opacity-15","style":"solid","width":"1px"}}}} /-->
 
-// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-echo $header_content;
-?>
+	<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2"} -->
+		<!-- wp:site-title {"level":0,"fontSize":"small"} /-->
+		<!-- wp:navigation {"menuSlug":"site-header-menu", "icon":"menu","backgroundColor": "charcoal-2", "overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small"} /-->
+	<!-- /wp:wporg/local-navigation-bar -->
+
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"18px","bottom":"18px","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}},"backgroundColor":"white","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:18px;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:18px;padding-left:var(--wp--preset--spacing--edge-space)">
+		<!-- wp:wporg/site-breadcrumbs {"fontSize":"small"} /-->
+	</div>
+	<!-- /wp:group -->
+BLOCKS
+);
+
+$current_page_title = apply_filters( 'wporg_translate_page_title', '' );
+$html_title         = implode( ' | ', array( $current_page_title, __( 'Translation Events', 'wporg-translate-events-2024' ) ) );
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title><?php echo esc_html( $html_title ); ?></title>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+		<div class="wp-site-blocks">
+			<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<?php echo $header; ?>
+			<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:18px;padding-left:var(--wp--preset--spacing--edge-space)">
+				<h2 class="wp-block-heading"><?php echo esc_html( $current_page_title ); ?></h2>
+
 			<?php echo $page_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		</div> <?php // Close wp-site-blocks div, opened in header block. ?>
 		<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
