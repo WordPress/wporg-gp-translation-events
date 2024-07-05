acbbb20b66e349719ad311c6f7b80d79f90e8151
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-05 12:15:00 +0100

    Remove unused functions

--- themes/wporg-translate-events-2024/inc/templates.php
+++ themes/wporg-translate-events-2024/inc/templates.php
@@ -3,8 +3,6 @@
 namespace Wporg\TranslationEvents\Theme_2024;
 
 class Templates {
-	private static string $theme_dir = __DIR__ . '/../';
-
 	public static function page( string $name, array $attributes = array() ): string {
 		$page_content       = self::block( "wporg-translate-events-2024/events-pages-$name", $attributes );
 		$page_title_content = self::block( 'wporg-translate-events-2024/page-title' );
@@ -17,19 +15,7 @@ class Templates {
 		return $header_content . $page_title_content . $page_content . $footer_content;
 	}
 
-	public static function part( string $name ): string {
-		ob_start();
-		include self::$theme_dir . "/parts/$name.html";
-		$content = ob_get_clean();
-		return do_blocks( $content );
-	}
-
-	public static function pattern( string $name ): string {
-		$json = wp_json_encode( array( 'slug' => $name ) );
-		return do_blocks( "<!-- wp:pattern $json /-->" );
-	}
-
-	public static function block( string $name, array $data = array() ): string {
+	private static function block( string $name, array $data = array() ): string {
 		$json = empty( $data ) ? '{}' : wp_json_encode( $data );
 		return do_blocks( "<!-- wp:$name $json /-->" );
 	}
