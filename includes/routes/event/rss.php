0a7cdd217ac6f0fd2623d24ebc8b3920f3583cf1
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2024-07-05 10:09:59 +0200

    Add the http://purl.org/rss/1.0/modules/event/ namespace to be able to have startdate and enddate in each event

--- includes/routes/event/rss.php
+++ includes/routes/event/rss.php
@@ -2,6 +2,7 @@
 
 namespace Wporg\TranslationEvents\Routes\Event;
 
+use DateTimeInterface;
 use Wporg\TranslationEvents\Event\Event;
 use Wporg\TranslationEvents\Event\Event_Repository_Interface;
 use Wporg\TranslationEvents\Routes\Route;
@@ -28,7 +29,7 @@ class Rss_Route extends Route {
 	 * @return void
 	 */
 	public function handle(): void {
-		$current_events_query = $this->event_repository->get_all_events( 1, 20 );
+		$current_events_query = $this->event_repository->get_current_and_upcoming_events( 1, 20 );
 		$rss_feed             = $this->get_rss_20_header( $current_events_query->events );
 
 		foreach ( $current_events_query->events as $event ) {
@@ -48,7 +49,7 @@ class Rss_Route extends Route {
 	 * @return string
 	 */
 	private function get_rss_20_header( array $events ): string {
-		$header  = '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">';
+		$header  = '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:ev="http://purl.org/rss/1.0/modules/event/">';
 		$header .= '	<channel>';
 		$header .= '		<title>' . esc_html__( 'WordPress.org Global Translation Events', 'gp-translation-events' ) . '</title>';
 		$header .= '		<link>' . esc_url( home_url( gp_url( '/events' ) ) ) . '</link>';
@@ -79,7 +80,9 @@ class Rss_Route extends Route {
 		$item .= '			<link>' . esc_url( home_url( gp_url( gp_url_join( 'events', $event->slug() ) ) ) ) . '</link>';
 		$item .= '			<description>' . esc_html( $event->description() ) . '</description>';
 		$item .= '          <enclosure url="' . esc_url( Urls::event_image( $event->id() ) ) . '" type="image/png" length="1200" />';
-		$item .= '			<pubDate>' . esc_html( $event->created_at()->format( DATE_RSS ) ) . '</pubDate>';
+		$item .= '			<pubDate>' . esc_html( $event->updated_at()->format( DATE_RSS ) ) . '</pubDate>';
+		$item .= '			<ev:startdate>' . esc_html( $event->start()->format( DateTimeInterface::ATOM ) ) . '</ev:startdate>';
+		$item .= '			<ev:enddate>' . esc_html( $event->end()->format( DateTimeInterface::ATOM ) ) . '</ev:enddate>';
 		$item .= '			<guid>' . esc_url( home_url( gp_url( gp_url_join( 'events', $event->slug() ) ) ) ) . '</guid>';
 		$item .= '		</item>';
 		return $item;
@@ -97,11 +100,11 @@ class Rss_Route extends Route {
 			return null;
 		}
 
-		$pub_date = $events[0]->created_at()->format( DATE_RSS );
+		$pub_date = $events[0]->updated_at()->format( DATE_RSS );
 
 		foreach ( $events as $event ) {
-			if ( strtotime( $event->created_at()->format( DATE_RSS ) ) > strtotime( $pub_date ) ) {
-				$pub_date = $event->created_at()->format( DATE_RSS );
+			if ( strtotime( $event->updated_at()->format( DATE_RSS ) ) > strtotime( $pub_date ) ) {
+				$pub_date = $event->updated_at()->format( DATE_RSS );
 			}
 		}
 
@@ -114,11 +117,11 @@ class Rss_Route extends Route {
 			return null;
 		}
 
-		$last_build_date = $events[0]->created_at()->format( DATE_RSS );
+		$last_build_date = $events[0]->updated_at()->format( DATE_RSS );
 
 		foreach ( $events as $event ) {
-			if ( strtotime( $event->created_at()->format( DATE_RSS ) ) > strtotime( $last_build_date ) ) {
-				$last_build_date = $event->created_at()->format( DATE_RSS );
+			if ( strtotime( $event->updated_at()->format( DATE_RSS ) ) > strtotime( $last_build_date ) ) {
+				$last_build_date = $event->updated_at()->format( DATE_RSS );
 			}
 		}
 
