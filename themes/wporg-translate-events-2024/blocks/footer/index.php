57a5497ad86d0a09c076f1ef2d051de621292b8f
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-11 14:45:00 +0100

    Don't do do_blocks() calls in the footer's render.php

--- themes/wporg-translate-events-2024/blocks/footer/index.php
+++ themes/wporg-translate-events-2024/blocks/footer/index.php
@@ -9,7 +9,7 @@ register_block_type(
 		'render_callback' => function ( array $attributes ) {
 			ob_start();
 			include_once __DIR__ . '/render.php';
-			return ob_get_clean();
+			return do_blocks( ob_get_clean() );
 		},
 	)
 );
--- themes/wporg-translate-events-2024/blocks/footer/render.php
+++ themes/wporg-translate-events-2024/blocks/footer/render.php
@@ -1,8 +1,7 @@
 <?php namespace Wporg\TranslationEvents\Theme_2024; ?>
 
 			</div><?php // Close the main wp-block-group div, opened by the header block. ?>
-			<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-			<?php echo do_blocks( '<!-- wp:wporg/global-footer /-->' ); ?>
+			<!-- wp:wporg/global-footer /-->
 			<?php wp_footer(); ?>
 		</div><?php // Close the wp-site-blocks div, opened by the header block. ?>
 	</body>
