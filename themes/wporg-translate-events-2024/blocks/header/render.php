bb8440e622ce881c6d25ff54b3ca8c8a0cc0167c
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-11 14:55:04 +0100

    Extract page header "pattern" to a separate file

--- themes/wporg-translate-events-2024/blocks/header/render.php
+++ themes/wporg-translate-events-2024/blocks/header/render.php
@@ -8,22 +8,9 @@ $html_title         = implode( ' | ', array( $current_page_title, __( 'Translati
 
 // The header content must be rendered before the call to wp_head() below,
 // so that styles and scripts of the referenced blocks are registered.
-$header = do_blocks(
-	<<<BLOCKS
-	<!-- wp:wporg/global-header {"style":{"border":{"bottom":{"color":"var:preset|color|white-opacity-15","style":"solid","width":"1px"}}}} /-->
-
-	<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2"} -->
-		<!-- wp:site-title {"level":0,"fontSize":"small"} /-->
-		<!-- wp:navigation {"menuSlug":"site-header-menu", "icon":"menu","backgroundColor": "charcoal-2", "overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small"} /-->
-	<!-- /wp:wporg/local-navigation-bar -->
-
-	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"18px","bottom":"18px","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}},"backgroundColor":"white","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:18px;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:18px;padding-left:var(--wp--preset--spacing--edge-space)">
-		<!-- wp:wporg/site-breadcrumbs {"fontSize":"small"} /-->
-	</div>
-	<!-- /wp:group -->
-BLOCKS
-);
+ob_start();
+require __DIR__ . '/page-header.php';
+$header = do_blocks( ob_get_clean() );
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
