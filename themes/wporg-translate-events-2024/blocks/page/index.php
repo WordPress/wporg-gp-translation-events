238f64c8670ef4e01016cba4a0cc28d3dcd8daab
Paulo Pinto <paulo.pinto@automattic.com>
2024-07-11 16:04:35 +0100

    Move generating the page content to index.php

--- themes/wporg-translate-events-2024/blocks/page/index.php
+++ themes/wporg-translate-events-2024/blocks/page/index.php
@@ -7,6 +7,12 @@ register_block_type(
 		// because otherwise it won't be available in render.php.
 		// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
 		'render_callback' => function ( array $attributes ) {
+			$page_name       = $attributes['page_name'];
+			$page_attributes = $attributes['page_attributes'];
+
+			// First render the block of the given page, so that it modifies title, breadcrumbs, etc.
+			$page_content = do_blocks( "<!-- wp:wporg-translate-events-2024/pages-$page_name " . wp_json_encode( $page_attributes ) . ' /-->' );
+
 			ob_start();
 			include_once __DIR__ . '/render.php';
 			return ob_get_clean();
--- themes/wporg-translate-events-2024/blocks/page/render.php
+++ themes/wporg-translate-events-2024/blocks/page/render.php
@@ -1,12 +1,6 @@
 <?php namespace Wporg\TranslationEvents\Theme_2024;
 
-/** @var array $attributes */
-
-$page_name       = $attributes['page_name'];
-$page_attributes = $attributes['page_attributes'];
-
-// First render the block of the given page, so that it modifies title, breadcrumbs, etc.
-$page_content = do_blocks( "<!-- wp:wporg-translate-events-2024/pages-$page_name " . wp_json_encode( $page_attributes ) . ' /-->' );
+/** @var array $page_content */
 
 // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 ?><?php echo do_blocks( '<!-- wp:wporg-translate-events-2024/header /-->' ); ?>
