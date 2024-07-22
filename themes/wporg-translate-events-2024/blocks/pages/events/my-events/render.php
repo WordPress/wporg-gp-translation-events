bda00a97562d6099e7dd0653564a0bf97cac0666
Tosin Oguntuyi <tcubed03@gmail.com>
2024-07-22 22:35:32 +0100

    Add block for attendance mode

--- themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
+++ themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
@@ -13,8 +13,9 @@ foreach ( $translation_events->events as $event ) {
 	$translation_events_lookup[ $event->id() ] = $event;
 
 	?>
-	<!-- wp:wporg-translate-events-2024/components-event-title <?php echo json_encode( array('id'=>$event->id())); ?> /-->
-	<!-- wp:wporg-translate-events-2024/components-event-start <?php echo json_encode( array('id'=>$event->id())); ?> /-->
+	<!-- wp:wporg-translate-events-2024/components-event-title <?php echo json_encode( array( 'id' => $event->id() ) ); ?> /-->
+	<!-- wp:wporg-translate-events-2024/components-event-attendance-mode <?php echo json_encode( array( 'id' => $event->id() ) ); ?> /-->
+	<!-- wp:wporg-translate-events-2024/components-event-start <?php echo json_encode( array( 'id' => $event->id() ) ); ?> /-->
 	<?php
 }
 ?>
