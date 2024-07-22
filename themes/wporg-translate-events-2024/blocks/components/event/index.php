9533067769f09b222a20e1f9066c93757c45353b
Tosin Oguntuyi <tcubed03@gmail.com>
2024-07-22 22:36:39 +0100

    Register block for attendance mode

--- themes/wporg-translate-events-2024/blocks/components/event/index.php
+++ themes/wporg-translate-events-2024/blocks/components/event/index.php
@@ -24,3 +24,15 @@ register_block_type(
 		},
 	)
 );
+register_block_type(
+	'wporg-translate-events-2024/components-event-attendance-mode',
+	array(
+		// The $attributes argument cannot be removed despite not being used in this function,
+		// because otherwise it won't be available in render.php.
+		// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
+		'render_callback' => function ( array $attributes ) {
+			global $translation_events_lookup;
+			return '<div>' . esc_html( $translation_events_lookup[ $attributes['id'] ]->attendance_mode() ) . '</div>';
+		},
+	)
+);
