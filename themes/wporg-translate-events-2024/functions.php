87a9cc9cc94d5cd591439f9f59ea88809818e8f1
Tosin Oguntuyi <tcubed03@gmail.com>
2024-07-26 13:14:56 +0100

    Fix lint error

--- themes/wporg-translate-events-2024/functions.php
+++ themes/wporg-translate-events-2024/functions.php
@@ -12,7 +12,6 @@ function register_blocks(): void {
 	include_once __DIR__ . '/blocks/footer/index.php';
 	include_once __DIR__ . '/blocks/pages/events/my-events/index.php';
 	include_once __DIR__ . '/blocks/components/event/index.php';
-
 }
 
 function register_patterns(): void {
