f77f5d372729cc847cd9ace83815595ebfa377b0
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2024-07-31 07:50:16 +0200

    Change json_encode to wp_json_encode

--- themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
+++ themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
@@ -5,8 +5,8 @@ $event_ids = $attributes['event_ids'] ?? array();
 
 foreach ( $event_ids as $event_id ) {
 	?>
-	<!-- wp:wporg-translate-events-2024/title <?php echo json_encode( array( 'id' => $event_id ) ); ?> /-->
-	<!-- wp:wporg-translate-events-2024/start-date <?php echo json_encode( array( 'id' => $event_id ) ); ?> /-->
+	<!-- wp:wporg-translate-events-2024/title <?php echo wp_json_encode( array( 'id' => $event_id ) ); ?> /-->
+	<!-- wp:wporg-translate-events-2024/start-date <?php echo wp_json_encode( array( 'id' => $event_id ) ); ?> /-->
 	<?php
 }
 ?>
