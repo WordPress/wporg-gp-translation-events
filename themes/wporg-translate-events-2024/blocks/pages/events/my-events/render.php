69e74dcae36a3a0d54a5dba2d0bef43b5ddda262
Tosin Oguntuyi <tcubed03@gmail.com>
2024-08-01 16:20:40 +0100

    Update my-events page to use new list block

--- themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
+++ themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
@@ -3,9 +3,7 @@ namespace Wporg\TranslationEvents\Theme_2024;
 
 $event_ids = $attributes['event_ids'] ?? array();
 
-/** @var Events_Query_Result $events */
-$events = $attributes['events'] ?? array();
-
-global $translation_events;
+$current_user_attendee_per_event = $attributes['current_user_attendee_per_event'] ?? array();
 ?>
-<!-- wp:pattern {"slug":"wporg-translate-events-2024/events-list"} /-->
+<!-- wp:wporg-translate-events-2024/component-event-list <?php echo wp_json_encode( array( 'event_ids' => $event_ids, 'current_user_attendee_per_event' => $current_user_attendee_per_event  ) ); ?>  /-->
+
