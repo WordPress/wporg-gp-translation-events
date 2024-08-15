5b14fd86f8e7922ecfea684806467b2e68a88394
Tosin Oguntuyi <tcubed03@gmail.com>
2024-08-15 06:06:02 +0100

    Use attribute

--- themes/wporg-translate-events-2024/blocks/event-excerpt/index.php
+++ themes/wporg-translate-events-2024/blocks/event-excerpt/index.php
@@ -7,7 +7,7 @@ register_block_type(
 		// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
 		'render_callback' => function ( array $attributes ) {
 			$event = Translation_Events::get_event_repository()->get_event( $attributes['id'] );
-			return '<p>' . esc_html( get_the_excerpt( $event->id() ) ) . '</p>';
+			return '<p>' . esc_html( get_the_excerpt( $attributes['id'] ) ) . '</p>';
 		},
 	)
 );
