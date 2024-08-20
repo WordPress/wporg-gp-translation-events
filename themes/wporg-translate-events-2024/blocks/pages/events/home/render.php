872ceb625fb3355c4a92c8993c570afc6125c418
Tosin Oguntuyi <tcubed03@gmail.com>
2024-08-20 22:17:30 +0100

    Add list blocks for events to home page block

--- themes/wporg-translate-events-2024/blocks/pages/events/home/render.php
+++ themes/wporg-translate-events-2024/blocks/pages/events/home/render.php
@@ -1,4 +1,34 @@
 <?php
 namespace Wporg\TranslationEvents\Theme_2024;
+
+$event_ids = $attributes['event_ids'] ?? array();
+
+$current_events_data = array(
+	'event_ids' => $attributes['current_events_query']['event_ids'] ?? array(),
+	'show_flag' => false,
+);
+
+$upcoming_events_data = array(
+	'event_ids' => $attributes['upcoming_events_query']['event_ids'] ?? array(),
+	'show_flag' => false,
+);
+
+$past_events_data = array(
+	'event_ids' => $attributes['past_events_query']['event_ids'] ?? array(),
+	'show_flag' => false,
+);
+
 ?>
 <!-- wp:pattern {"slug":"wporg-translate-events-2024/front-cover"} /-->
+<!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Your next events</h4>
+<!-- /wp:heading -->
+<!-- wp:wporg-translate-events-2024/event-list <?php echo wp_json_encode( $current_events_data ); ?>  /-->
+<!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Upcoming events</h4>
+<!-- /wp:heading -->
+<!-- wp:wporg-translate-events-2024/event-list <?php echo wp_json_encode( $upcoming_events_data ); ?>  /-->
+<!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Past events</h4>
+<!-- /wp:heading -->
+<!-- wp:wporg-translate-events-2024/event-list <?php echo wp_json_encode( $past_events_data ); ?>  /-->
