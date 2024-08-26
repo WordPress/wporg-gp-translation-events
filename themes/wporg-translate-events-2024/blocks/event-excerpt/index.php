144e143dde5503dd8a1fdd34b45bda523abf8e8f
Tosin Oguntuyi <tcubed03@gmail.com>
2024-08-26 10:07:30 +0100

    Remove unused variable

--- themes/wporg-translate-events-2024/blocks/event-excerpt/index.php
+++ themes/wporg-translate-events-2024/blocks/event-excerpt/index.php
@@ -6,7 +6,6 @@ register_block_type(
 	array(
 		// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
 		'render_callback' => function ( array $attributes ) {
-			$event = Translation_Events::get_event_repository()->get_event( $attributes['id'] );
 			return '<p>' . esc_html( get_the_excerpt( $attributes['id'] ) ) . '</p>';
 		},
 	)
