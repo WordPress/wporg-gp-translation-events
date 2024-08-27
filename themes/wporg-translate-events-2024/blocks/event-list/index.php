d32cf4315368d4c8608dc42503ae6d6be084c701
Alex Kirk <akirk@users.noreply.github.com>
2024-08-27 17:48:26 +0200

    Use event-list again

--- themes/wporg-translate-events-2024/blocks/event-list/index.php
+++ themes/wporg-translate-events-2024/blocks/event-list/index.php
@@ -21,13 +21,18 @@ register_block_type(
 				<?php
 				foreach ( $event_ids as $event_id ) {
 					?>
-				<li class="wporg-marker-list-item">
-					<!-- wp:wporg-translate-events-2024/event-template <?php echo wp_json_encode( array( 'id' => $event_id ) ); ?> -->
-					<!-- wp:wporg-translate-events-2024/event-title /-->
-					<!-- wp:wporg-translate-events-2024/event-attendance-mode /-->
-					<!-- wp:wporg-translate-events-2024/event-start /-->
-					<!-- /wp:wporg-translate-events-2024/event-template -->
-				</li>
+					<li class="wporg-marker-list-item">
+						<!-- wp:wporg-translate-events-2024/event-template <?php echo wp_json_encode( array( 'id' => $event_id ) ); ?> -->
+						<div>
+							<!-- wp:wporg-translate-events-2024/event-title /-->
+							<!-- wp:wporg-translate-events-2024/event-flag /-->
+						</div>
+						<!-- wp:wporg-translate-events-2024/event-attendance-mode /-->
+						<!-- wp:wporg-translate-events-2024/event-start /-->
+						<!-- /wp:wporg-translate-events-2024/event-list-->
+
+						<!-- /wp:wporg-translate-events-2024/event-template -->
+					</li>
 					<?php
 				}
 				?>
--- themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
+++ themes/wporg-translate-events-2024/blocks/pages/events/my-events/render.php
@@ -4,25 +4,4 @@ namespace Wporg\TranslationEvents\Theme_2024;
 $event_ids = $attributes['event_ids'] ?? array();
 
 ?>
-<div class="wp-block-wporg-event-list">
-	<ul class="wporg-marker-list__container">
-		<?php
-		foreach ( $event_ids as $event_id ) {
-			?>
-			<li class="wporg-marker-list-item">
-				<!-- wp:wporg-translate-events-2024/event-template <?php echo wp_json_encode( array( 'id' => $event_id ) ); ?> -->
-				<div>
-					<!-- wp:wporg-translate-events-2024/event-title /-->
-					<!-- wp:wporg-translate-events-2024/event-flag /-->
-				</div>
-				<!-- wp:wporg-translate-events-2024/event-attendance-mode /-->
-				<!-- wp:wporg-translate-events-2024/event-start /-->
-				<!-- /wp:wporg-translate-events-2024/event-list-->
-
-				<!-- /wp:wporg-translate-events-2024/event-template -->
-			</li>
-			<?php
-		}
-		?>
-	</ul>
-</div>
+<!-- wp:wporg-translate-events-2024/event-list <?php echo wp_json_encode( array( 'event_ids' => $event_ids ) ); ?> /-->
