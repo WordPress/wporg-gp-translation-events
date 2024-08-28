ac3e76aeeb2a699d1ddfb2e3013f773c5ab973e1
Tosin Oguntuyi <tcubed03@gmail.com>
2024-08-28 14:55:33 +0100

    Style links in box

--- themes/wporg-translate-events-2024/patterns/front-cover.php
+++ themes/wporg-translate-events-2024/patterns/front-cover.php
@@ -9,20 +9,20 @@ ob_start();
  <!-- wp:paragraph -->
 <p>Start your journey to contribute to localization.</p>
 <!-- /wp:paragraph -->
-<div class="wp-block-columns alignwide" style="gap:10px;"><!-- wp:column {"width":"100%","fontSize":"medium","fontFamily":"eb-garamond"} -->
-<div class="wp-block-column has-eb-garamond-font-family has-medium-font-size" style="flex-basis:100%"><!-- wp:group {"style":{"border":{"radius":"2px","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"borderColor":"light-grey-1","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px;border-radius:2px;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-0"}}}},"textColor":"charcoal-0","fontSize":"heading-6"} -->
-<a href="/events/">Host an event</a>
-<p class="has-charcoal-0-color has-text-color has-link-color has-heading-6-font-size">Create your own translation event.</p>
+<div class="wp-block-columns alignwide" style="gap:10px;"><!-- wp:column {"width":"100%","fontSize":"medium","fontFamily":"inter"} -->
+<div class="wp-block-column has-inter-font-family" style="flex-basis:100%"><!-- wp:group {"style":{"border":{"radius":"2px","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"borderColor":"light-grey-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px;border-radius:2px;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-0"}}}},"textColor":"charcoal-0"} -->
+<a class="blue-bolded-link" href="/events/">Host an event</a>
+<p class="has-charcoal-0-color has-text-color has-link-color ">Create your own translation event.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
-		
-<!-- wp:column {"width":"100%","fontSize":"medium","fontFamily":"eb-garamond"} -->
-<div class="wp-block-column has-eb-garamond-font-family has-medium-font-size" style="flex-basis:100%"><!-- wp:group {"style":{"border":{"radius":"2px","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"borderColor":"light-grey-1","layout":{"type":"constrained"}} -->
+
+<!-- wp:column {"width":"100%","fontSize":"medium","fontFamily":"inter"} -->
+<div class="wp-block-column has-inter-font-family" style="flex-basis:100%"><!-- wp:group {"style":{"border":{"radius":"2px","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30"}}},"borderColor":"light-grey-1","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px;border-radius:2px;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|charcoal-0"}}}},"textColor":"charcoal-0","fontSize":"heading-6"} -->
-<a href="/events/">My events</a>
-<p class="has-charcoal-0-color has-text-color has-link-color has-heading-6-font-size">Manage events you’ve created, or pledged to attend.</p>
+<a class="wp-block-heading has-inter-font-family blue-bolded-link" href="/events/">My events</a>
+<p class="has-charcoal-0-color has-text-color has-link-color">Manage events you’ve created, or pledged to attend.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
