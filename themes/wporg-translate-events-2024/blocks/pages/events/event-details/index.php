e7677ef812bfa296f7e3abd4e940186babdeb87b
Tosin Oguntuyi <tcubed03@gmail.com>
2024-09-12 15:07:42 +0100

    Add event title to breadcrumb

--- themes/wporg-translate-events-2024/blocks/pages/events/event-details/index.php
+++ themes/wporg-translate-events-2024/blocks/pages/events/event-details/index.php
@@ -1,19 +1,22 @@
 <?php
 namespace Wporg\TranslationEvents\Theme_2024;
+
 use Wporg\TranslationEvents\Translation_Events;
 
 register_block_type(
 	'wporg-translate-events-2024/page-events-event-details',
 	array(
 		'render_callback' => function ( array $attributes ) {
+			$event_id = $attributes['event_id'] ?? array();
+			$event = Translation_Events::get_event_repository()->get_event( $event_id );
 			add_filter(
 				'wporg_block_site_breadcrumbs',
-				function ( $breadcrumbs ): array {
+				function ( $breadcrumbs ) use( $event ): array {
 					return array_merge(
 						$breadcrumbs,
 						array(
 							array(
-								'title' => __( 'Event', 'wporg-translate-events-2024' ),
+								'title' => esc_html( $event->title() ),
 								'url'   => null,
 							),
 						)
@@ -21,8 +24,6 @@ register_block_type(
 				}
 			);
 
-			$event_id = $attributes['event_id'] ?? array();
-			$event = Translation_Events::get_event_repository()->get_event( $event_id );
 			if ( ! $event ) {
 				return '';
 			}
