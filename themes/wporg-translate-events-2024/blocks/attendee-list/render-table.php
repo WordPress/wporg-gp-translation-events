6fe95e8945cd706861e5a5b69254fda7e46aeec2
Tosin Oguntuyi <tcubed03@gmail.com>
2024-09-18 12:35:52 +0100

    Style buttons

--- themes/wporg-translate-events-2024/blocks/attendee-list/render-table.php
+++ themes/wporg-translate-events-2024/blocks/attendee-list/render-table.php
@@ -7,10 +7,10 @@ use Wporg\TranslationEvents\Urls;
 					<table class="event-stats">
 						<thead>
 							<tr>
-                                <th><?php esc_html_e( 'Name', 'wporg-translate-events-2024' ); ?></th>
-                                <th><?php esc_html_e( 'Remote', 'wporg-translate-events-2024' ); ?></th>
-                                <th><?php esc_html_e( 'Host', 'wporg-translate-events-2024' ); ?></th>
-                                <th><?php esc_html_e( 'Action', 'wporg-translate-events-2024' ); ?></th>
+								<th><?php esc_html_e( 'Name', 'wporg-translate-events-2024' ); ?></th>
+								<th><?php esc_html_e( 'Remote', 'wporg-translate-events-2024' ); ?></th>
+								<th><?php esc_html_e( 'Host', 'wporg-translate-events-2024' ); ?></th>
+								<th><?php esc_html_e( 'Action', 'wporg-translate-events-2024' ); ?></th>
 							</tr>
 						</thead>
 						<tbody>
@@ -29,28 +29,30 @@ use Wporg\TranslationEvents\Urls;
 									/-->
 							</td>
 							<td>
-                                <?php if ( $attendee->is_remote() ) : ?>
-                                    <span><?php esc_html_e( 'Yes', 'wporg-translate-events-2024' ); ?></span>
-                                    <?php endif; ?>
-                            </td>
-                            <td>
-						<?php if ( $attendee->is_host() ) : ?>
+								<?php if ( $attendee->is_remote() ) : ?>
+									<span><?php esc_html_e( 'Yes', 'wporg-translate-events-2024' ); ?></span>
+									<?php endif; ?>
+							</td>
+							<td>
+							<?php if ( $attendee->is_host() ) : ?>
 							<span><?php esc_html_e( 'Yes', 'wporg-translate-events-2024' ); ?></span>
 							<?php endif; ?>
 					</td>
 					<td>
 					<form class="add-remove-user-as-host" method="post" action="<?php echo esc_url( Urls::event_toggle_host( $event->id(), $attendee->user_id() ) ); ?>">
-						<?php if ( $attendee->is_host() ) : ?>
-							<input type="submit" class="button is-primary remove-as-host" value="<?php echo esc_attr__( 'Remove as host', 'wporg-translate-events-2024' ); ?>"/>
+					<div class="wp-block-buttons wporg-theme-actions is-layout-flex wp-block-buttons-is-layout-flex">
+							<?php if ( $attendee->is_host() ) : ?>
+							<input type="submit" class="wp-block-button__link remove-as-host" value="<?php echo esc_attr__( 'Remove as host', 'wporg-translate-events-2024' ); ?>"/>
 							<?php else : ?>
-									<input type="submit" class="button is-secondary convert-to-host" value="<?php echo esc_attr__( 'Make co-host', 'wporg-translate-events-2024' ); ?>"/>
+									<input type="submit" class="wp-block-button__link convert-to-host" value="<?php echo esc_attr__( 'Make co-host', 'wporg-translate-events-2024' ); ?>"/>
 							<?php endif; ?>
 							<?php if ( $event->is_hybrid() ) : ?>
-								<a href="<?php echo esc_url( Urls::event_toggle_attendance_mode( $event->id(), $attendee->user_id() ) ); ?>" class="button set-attendance-mode"><?php $attendee->is_remote() ? esc_html_e( 'Set as on-site', 'wporg-translate-events-2024' ) : esc_html_e( 'Set as remote', 'wporg-translate-events-2024' ); ?></a>
+								<div class="wp-block-button is-style-outline"><a href="<?php echo esc_url( Urls::event_toggle_attendance_mode( $event->id(), $attendee->user_id() ) ); ?>" class="wp-block-button__link wp-element-button set-attendance-mode" id="wporg-theme-button-preview"><?php $attendee->is_remote() ? esc_html_e( 'Set as on-site', 'wporg-translate-events-2024' ) : esc_html_e( 'Set as remote', 'wporg-translate-events-2024' ); ?></a></div>
 							<?php endif; ?>
 							<?php if ( ! $attendee->is_host() ) : ?>
-								<a href="<?php echo esc_url( Urls::event_remove_attendee( $event->id(), $attendee->user_id() ) ); ?>" class="button remove-attendee"><?php esc_html_e( 'Remove', 'wporg-translate-events-2024' ); ?></a>
+								<div class="wp-block-button is-style-outline"><a href="<?php echo esc_url( Urls::event_remove_attendee( $event->id(), $attendee->user_id() ) ); ?>" class="wp-block-button__link wp-element-button remove-attendee" id="wporg-theme-button-preview"><?php esc_html_e( 'Remove', 'wporg-translate-events-2024' ); ?></a></div>
 							<?php endif; ?>
+					</div>
 						</form>
 					</td>
 							</tr>
