60d72037205dc008222a9db14fc2b8ae383ec299
Tosin Oguntuyi <tcubed03@gmail.com>
2024-10-07 15:10:19 +0100

    Style nav links

--- themes/wporg-translate-events-2024/blocks/event-nav-links/index.php
+++ themes/wporg-translate-events-2024/blocks/event-nav-links/index.php
@@ -13,14 +13,19 @@ register_block_type(
 			?>
 			<!-- wp:group {"align":"right","layout":{"type":"flex","justifyContent":"right"}} -->
 			<div class="wp-block-group alignright" style="width: 35%; margin-left: auto; padding-right:var(--wp--preset--spacing--edge-space);">
-				<div class="wp-block-group__inner-container" style="display: flex; justify-content: flex-end; gap: 10px;">
+				<div class="wp-block-group__inner-container" style="display: flex; justify-content: flex-end; gap: 10px; align-items: center;">
 					<?php if ( is_user_logged_in() ) : ?>
 						<?php if ( current_user_can( 'manage_translation_events' ) ) : ?>
-						<a href="<?php echo esc_url( Urls::events_trashed() ); ?>">Deleted Events</a>
+						<a class="event-nav-link" href="<?php echo esc_url( Urls::events_trashed() ); ?>">Deleted Events</a>
 					<?php endif; ?>
-					<a href="<?php echo esc_url( Urls::my_events() ); ?>">My Events</a>
+					<a class="event-nav-link" href="<?php echo esc_url( Urls::my_events() ); ?>">My Events</a>
 						<?php if ( current_user_can( 'create_translation_event' ) ) : ?>
-						<a class="button is-primary" href="<?php echo esc_url( Urls::event_create() ); ?>">Create Event</a>
+							<!-- wp:button {"className":"is-style-outline"} -->
+							<div class="wp-block-button is-style-outline">
+								<a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( Urls::event_create() ); ?>">Create Event</a>
+							</div>
+							<!-- /wp:button -->
+						<!-- <a class="button is-primary" href="">Create Event</a> -->
 					<?php endif; ?>
 				<?php endif; ?>
 				</div>
