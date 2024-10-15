84c35c3aca4e6b6ad8239714459702fb010d7889
Tosin Oguntuyi <tcubed03@gmail.com>
2024-10-15 17:57:44 +0100

    Create trashed events page block

--- themes/wporg-translate-events-2024/functions.php
+++ themes/wporg-translate-events-2024/functions.php
@@ -35,6 +35,7 @@ function register_blocks(): void {
 	include_once __DIR__ . '/blocks/event-trash-link/index.php';
 	include_once __DIR__ . '/blocks/event-nav-links/index.php';
 	include_once __DIR__ . '/blocks/event-load-more-button/index.php';
+	include_once __DIR__ . '/blocks/pages/events/trashed-events/index.php';
 }
 
 function register_patterns(): void {
