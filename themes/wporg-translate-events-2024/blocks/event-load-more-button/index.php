61aa6a79a0b18af82613522de1b9c99cce33ad78
Tosin Oguntuyi <tcubed03@gmail.com>
2024-11-22 18:32:28 +0100

    Remove unused parameters

--- themes/wporg-translate-events-2024/blocks/event-load-more-button/index.php
+++ themes/wporg-translate-events-2024/blocks/event-load-more-button/index.php
@@ -7,7 +7,7 @@ register_block_type(
 	'wporg-translate-events-2024/event-load-more-button',
 	array(
 		// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
-		'render_callback' => function ( array $attributes, $content, $block ) {
+		'render_callback' => function () {
 			ob_start();
 			?>
 			<!-- wp:button {"className":"is-style-outline"} -->
