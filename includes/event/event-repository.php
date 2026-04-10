53437d90385f8cdb0d2308b80dcf035528fdf822
Dion Hulse <dion@wordpress.org>
2026-04-10 18:12:12 +1000

    PHP 8 compatibility fixes: Use the nullable int type.

--- includes/event/event-repository.php
+++ includes/event/event-repository.php
@@ -509,7 +509,7 @@ class Event_Repository {
 		int $page_size,
 		array $args,
 		array $filter_by_ids = array(),
-		int $user_id = null,
+		?int $user_id = null,
 		bool $include_created_by_user = false
 	): Events_Query_Result {
 		$this->assert_pagination_arguments( $page, $page_size );
